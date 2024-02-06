304ce4a4e016ce558b3986a4bd48c14a8ac83913
chdenat <christian.denat@orange.fr>
2024-02-06 20:03:55 +0100

    - manage gpx, kml and geojson file types

--- src/Utils/TrackUtils.js
+++ src/Utils/TrackUtils.js
@@ -1,8 +1,9 @@
-import {Color, GeoJsonDataSource, GpxDataSource, KmlDataSource} from 'cesium'
-import {FileUtils}                                              from './FileUtils.js'
-import {UINotifier}                                             from './UINotifier'
+import {gpx, kml}                 from '@tmcw/togeojson'
+import {Color, GeoJsonDataSource} from 'cesium'
+import {FileUtils}                from './FileUtils.js'
+import {UINotifier}               from './UINotifier'
 
-export const ACCEPTED_TRACK_FILES = ['.geojson', '.kml', '.gpx', '.kmz']
+export const ACCEPTED_TRACK_FILES = ['.geojson', '.kml', '.gpx' /* TODO '.kmz'*/]
 
 export class TrackUtils {
 
@@ -10,7 +11,7 @@ export class TrackUtils {
         gpx: ['application/gpx+xml', 'vnd.gpxsee.map+xml', 'application/octet-stream'],
         geojson: ['application/geo+json', 'application/json'],
         kml: ['vnd.google-earth.kml+xml'],
-        kmz: ['vnd.google-earth.kmz'],
+        // kmz: ['vnd.google-earth.kmz'], //TODO KMZ files
     }
 
     /**
@@ -35,8 +36,17 @@ export class TrackUtils {
      */
     static showTrack = async (track) => {
         let dataSource
-        const trackColor = Color.DARKRED
-        const routeColor = Color.DARKBLUE
+        const configuration = window.vt3d.configuration
+
+        const trackStroke = {
+            color: Color.fromCssColorString(configuration.track.color),
+            width: configuration.track.width,
+        }
+        const routeStroke = {
+            color: Color.fromCssColorString(configuration.route.color),
+            width: configuration.route.width,
+        }
+
         const commonOptions = {
             clampToGround: true,
             name: track.name,
@@ -45,59 +55,43 @@ export class TrackUtils {
          * Load DataSource and add it
          *
          * Available formats are : gpx,kml,kmz,geojson
+         *
+         * For convenient reasons, we translate kml and gpx to GeoJson formt
+         *
          */
         try {
+            let content = ''
             switch (track.extension) {
-                case 'gpx' : {
-                    const parser = new DOMParser()
-                    const content = parser.parseFromString(track.content, 'text/xml')
-                    dataSource = await GpxDataSource.load(content, {
-                        ...commonOptions,
-                        trackColor: trackColor,
-                        routeColor: routeColor,
-                    })
-                    break
-                }
-                case 'kml':
-                case 'kmz' : {
-
-                    const parser = new DOMParser()
-                    const content = parser.parseFromString(track.content, 'text/xml')
-                    console.log(parser)
-                    dataSource = await KmlDataSource.load(content, {
-                        ...commonOptions,
-                        camera: window.vt3DContext.camera,
-                        canvas: window.vt3DContext.scene.canvas,
-                        screenOverlayContainer: window.vt3DContext.viewer?.container,
-                    })
-
-                    break
-                }
                 case 'gpx':
-                case 'kml':
-            }
-                ':
-                    console.log(JSON.parse(track.content))
-                    dataSource = await GeoJsonDataSource.load(JSON.parse(track.content), {
-                        ...options,
-                        stroke: trackColor,
-                    })
+                    content = gpx(new DOMParser().parseFromString(track.content, 'text/xml'))
                     break
+                case 'kmz' :
+                // TODO unzip to get kml. but what to do with the assets files that are sometimes embedded
+                case 'kml':
+                    content = kml(new DOMParser().parseFromString(track.content, 'text/xml'))
+                    break
+                case 'geojson' :
+                    content = JSON.parse(track.content)
             }
+            dataSource = await GeoJsonDataSource.load(content, {
+                ...commonOptions,
+                stroke: trackStroke.color,
+                strokeWidth: trackStroke.width,
+            })
 
             /**
              * Then add and display data source
              */
 
             //dataSource.name = `${track.name}.${track.extension}`
-            window.vt3DContext.viewer.dataSources.add(dataSource)
+            window.vt3d.viewer.dataSources.add(dataSource)
                 .then(function (dataSource) {
                     // Ok => we notify
                     UINotifier.notifySuccess({
                         caption: 'Loaded!',
                         text: `The track <strong>${track.name}</strong> has been loaded and displayed on the map.`,
                     })
-                    window.vt3DContext.viewer.zoomTo(dataSource.entities)
+                    window.vt3d.viewer.zoomTo(dataSource.entities)
                 }).catch(error => {
                 // Error => we notify
                 UINotifier.notifyError({
@@ -106,6 +100,7 @@ export class TrackUtils {
                 })
             })
         } catch (error) {
+            console.error(error)
             // Error => we notify
             UINotifier.notifyError({
                 caption: `An error occurs during loading <strong>${track.name}<strong>!`,
