f83dddf4640bd0db40b5d7afed4d5a2cbeccb35d
chdenat <christian.denat@orange.fr>
2024-02-06 12:01:00 +0100

    - enhance comments

--- src/Utils/FileUtils.js
+++ src/Utils/FileUtils.js
@@ -7,26 +7,23 @@ export class FileUtils {
      * Upload a file corresponding to the desired types
      *
      *
-     * @returns {Promise<FileStringResult>}
+     * @returns {Promise<FileStringResult|string>}
      */
     static async uploadFileFromFrontEnd(props = null) {
         return await getFileAsString(
             props.accepted ? {acceptedExtensions: props.accepted} : {},
         ).then(file => {
-
             // Let's add extension info to file and change name by removing it
             const info = FileUtils.getFileNameAndExtension(file.name)
             file.name = info.name
             file.extension = info.extension
 
             // If no types specified, we force one type if mimes are specified
-            // else we use extension as types.
-            // Then we return the file object
+            // else we use extension as types then we return the file object
             if (file.type === '' && props.mimes) {
                 file.type = props.mimes[file.extension][0]
                 return file
             }
-
             // Else, Let's check for the type found in content to avoid hacking
             if (file.type === extension || (props.mimes && props.mimes[file.extension].includes(file.type))) {
                 return file
