7a07ec12550e70930ffac95ddf3f79702f8ebf25
chdenat <christian.denat@orange.fr>
2024-02-10 18:05:12 +0100

    - remove debug

--- src/Components/UI/CreditsUI/CreditsUI.jsx
+++ src/Components/UI/CreditsUI/CreditsUI.jsx
@@ -6,7 +6,6 @@ import {FA2SL}                                                          from '..
 import info                                                             from '../../../version.json'
 import './style.css'
 
-console.log(SlButton)
 export const CreditsUI = forwardRef(function CreditsUI(props, ref) {
     const [open, setOpen] = useState(false)
 
@@ -22,7 +21,7 @@ export const CreditsUI = forwardRef(function CreditsUI(props, ref) {
                     <strong>{window.vt3d.configuration.applicationName}</strong><span>{info.version}</span>
                 </div>
                 {/*<SlTooltip content="Our GitHub repo">*/}
-                <SlIconButton library="fab" name={FA2SL.set(faGithub)}
+                <SlIconButton library="fa" name={FA2SL.set(faGithub)}
                               target={'_blank'}
                               href={'https://github.com/ViewTrack3D/vt3d'}
                 />
