f142bba8c606c5f65ac3623a2c7c2c11ae2503c1
chdenat <christian.denat@orange.fr>
2024-02-17 19:03:46 +0100

    - use Track class

--- src/Components/UI/TrackFileLoaderUI/TrackFileLoaderUI.jsx
+++ src/Components/UI/TrackFileLoaderUI/TrackFileLoaderUI.jsx
@@ -2,46 +2,37 @@ import './style.css'
 import {faMapLocation}                             from '@fortawesome/pro-regular-svg-icons'
 import {SlButton, SlIcon, SlIconButton, SlTooltip} from '@shoelace-style/shoelace/dist/react'
 import {forwardRef}                                from 'react'
+import {Track}                                     from '../../../Classes/Track'
 import {FA2SL}                                     from '../../../Utils/FA2SL'
 import {TrackUtils as TU}                          from '../../../Utils/TrackUtils'
+import {AltitudeChoice}                            from '../Modals/AltitudeChoice'
 
 export const TrackFileLoaderUI = forwardRef(function TrackFileLoaderUI(props, ref) {
     const uploadFile = async () => {
         const track = await TU.loadTrackFromFile()
         // File is correct let's work with
         if (track !== undefined) {
-            // Let extract GeoJson
-            const geoJson = await TU.trackToGeoJson(track)
-            // Maybe we have some changes to operate
-            const newGeoJson = await TU.prepareGeoJson(geoJson)
-            // Get metrics
-            const metrics = await TU.getMetrics(newGeoJson)
-
-            // Let's add information to context
-            window.vt3d.addTrack({
-                content: newGeoJson,
-                name: track.name,
-                type: track.extension,
-                metrics: metrics,
-            })
-
-            // All's fine, the show continues :!
-            await TU.showTrack(newGeoJson)
+            let currentTrack = new Track(track.name, track.extension, track.content)
+            currentTrack.addToContext()
+            await currentTrack.show()
         }
     }
 
     return (
-        <div id="file-loader" className={'ui-element transparent'} ref={ref}>
-            <SlTooltip content="Load a track file">
-                <SlButton size="small" onClick={uploadFile}>
-                    <SlIcon library="fa" name={FA2SL.set(faMapLocation)} slot={'prefix'}/>
-                </SlButton>
-                <SlIconButton name="circle-check"
-                              target={'_blank'}
-                              href={'https://github.com/ViewTrack3D/vt3d'}
-                />
-            </SlTooltip>
-        </div>
+        <>
+            <div id="file-loader" className={'ui-element transparent'} ref={ref}>
+                <SlTooltip content="Load a track file">
+                    <SlButton size="small" onClick={uploadFile}>
+                        <SlIcon library="fa" name={FA2SL.set(faMapLocation)} slot={'prefix'}/>
+                    </SlButton>
+                    <SlIconButton name="circle-check"
+                                  target={'_blank'}
+                                  href={'https://github.com/ViewTrack3D/vt3d'}
+                    />
+                </SlTooltip>
+            </div>
+            <AltitudeChoice/>
+        </>
     )
 
 })
--- src/Utils/TrackUtils.js
+++ src/Utils/TrackUtils.js
@@ -1,9 +1,6 @@
-import {gpx, kml}                 from '@tmcw/togeojson'
 import * as Cesium                from 'cesium'
 import {Color, GeoJsonDataSource} from 'cesium'
-import {DateTime}                 from 'luxon'
 import {FileUtils}                from './FileUtils.js'
-import {Mobility}                 from './Mobility'
 import {UINotifier}               from './UINotifier'
 
 export const ACCEPTED_TRACK_FILES = ['.geojson', '.kml', '.gpx' /* TODO '.kmz'*/]
@@ -62,27 +59,12 @@ export class TrackUtils {
         const commonOptions = {
             clampToGround: true, name: name, markerSymbol: '<i>?</i>',
         }
-        /**
-         * Load DataSource and add it
-         *
-         * Available formats are : gpx,kml,kmz,geojson
-         *
-         * For convenient reasons, we translate kml and gpx to GeoJson formt
-         *
-         */
 
         try {
-
+            // Load Geo Json
             dataSource = await GeoJsonDataSource.load(geoJson, {
                 ...commonOptions, stroke: trackStroke.color, strokeWidth: trackStroke.width,
             })
-
-            // It's time to get metrics from
-            const metrics = await TrackUtils.getMetrics(geoJson)
-
-            /**
-             * Then add and display data source
-             */
 
             //dataSource.name = `${track.name}.${track.extension}`
             window.vt3d.viewer.dataSources.add(dataSource)
@@ -108,42 +90,6 @@ export class TrackUtils {
             })
         }
     }
-    /**
-     * Get the data and return a GeoJson
-     *
-     * @param trackFile
-     *
-     * @return {Array} geoJson
-     */
-    static trackToGeoJson = async (trackFile) => {
-        /**
-         * Available formats are : gpx,kml,kmz,geojson
-         * For convenient reasons, we translate kml and gpx to GeoJson format
-         */
-        try {
-            let geoJson
-            switch (trackFile.extension) {
-                case 'gpx':
-                    return gpx(new DOMParser().parseFromString(trackFile.content, 'text/xml'))
-                case 'kmz' :
-                // TODO unzip to get kml. but what to do with the assets files that are sometimes embedded
-                case 'kml':
-                    return kml(new DOMParser().parseFromString(trackFile.content, 'text/xml'))
-                case 'geojson' :
-                    geoJson = JSON.parse(trackFile.content)
-                    return geoJson
-            }
-
-        } catch (error) {
-            console.error(error)
-            // Error => we notify
-            UINotifier.notifyError({
-                caption: `An error occurs during loading <strong>${trackFile.name}<strong>!`, text: error,
-            })
-            return undefined
-        }
-    }
-
 
     /**
      * Filters an array of objects using custom predicates.
@@ -203,49 +149,6 @@ export class TrackUtils {
         }
         return dataExtract
     }
-    /**
-     * Prepare GeoJson
-     *
-     * Simulate height, interpolate, clean data
-     *
-     * @param geoJson
-     * @return geoJson
-     *
-     */
-    static prepareGeoJson = async geoJson => {
-
-        if (geoJson.type === FEATURE_COLLECTION) {
-            let index = 0
-            for (const feature of geoJson.features) {
-                if (feature.type === 'Feature' && feature.geometry.type === LINE_STRING) {
-
-                    const properties = TrackUtils.checkIfDataContainsHeightOrTime(feature)
-                    let index = 0
-                    const temp = []
-
-                    // Some heights info are missing. Let's simulate them
-
-                    // TODO add different plugins for DEM elevation like:
-                    //        https://tessadem.com/elevation-api/  ($)
-                    //     or https://github.com/Jorl17/open-elevation/blob/master/docs/api.md
-
-                    if (!properties.hasHeight) {
-                        const fixed = await TrackUtils.getElevationFromTerrain(feature.geometry.coordinates)
-                        for (let j = 0; j < fixed.length; j++) {
-                            feature.geometry.coordinates[j][2] = fixed[j]
-                        }
-                    }
-                    // TODO interpolate points to avoid GPS errors (Kalman Filter ?)
-                    // TODO Clean
-
-                    geoJson.features[index] = feature
-                }
-                index++
-            }
-        }
-        return geoJson
-    }
-
 
     /**
      * Get elevation from Cesium Terrain
@@ -268,120 +171,5 @@ export class TrackUtils {
         return height
 
     }
-
-    /**
-     * Compute all metrics from a track
-     *
-     * @param geoJson GeoJSON track
-     * @return {[metrics,global]}
-     */
-    static getMetrics = async (geoJson = []) => {
-        // We'll work on some of them
-        return await TrackUtils.prepareDataForMetrics(geoJson).then(dataForMetrics => {
-            let metrics = []
-            let index = 1
-
-            /**
-             * GeoJson is a Feature Collection, so we iterate on each.
-             */
-            dataForMetrics.forEach((dataSet) => {
-
-                let featureMetrics = []
-                /**
-                 * 1st step : Metrics per points
-                 * we iterate on all points to compute
-                 *  - distance, elevation, slope
-                 * If we have time information, we can also compute
-                 *  - duration, speed, pace
-                 */
-                for (const current of dataSet) {
-                    const prev = dataSet[index]
-                    const data = {}
-                    if (index < dataSet.length) {
-                        data.distance = Mobility.distance(prev, current)
-                        if (current.time && prev.time) {
-                            data.duration = Mobility.duration(DateTime.fromISO(prev.time), DateTime.fromISO(current.time))
-                            data.speed = Mobility.speed(data.distance, data.duration)
-                            data.pace = Mobility.pace(data.distance, data.duration)
-
-                            //TODO Add idle time duration
-                        }
-                        data.elevation = Mobility.elevation(prev, current)
-                        data.slope = data.elevation / data.distance * 100
-                    }
-                    index++
-                    featureMetrics.push(data)
-                }
-                featureMetrics = featureMetrics.slice(0, -1)
-
-                /**
-                 * Step 2: Globals
-                 *
-                 * Now we can compute globals, ie some min max, average + total information(distance, time, D+/D- )
-                 */
-                let global = {}, tmp = []
-
-                // Min Height
-                global.minHeight = Math.min(...dataSet.map(a => a?.height))
-
-                // Max Height
-                global.maxHeight = Math.max(...dataSet.map(a => a?.height))
-
-                // If the first have duration time, all the data set have time
-                if (featureMetrics[0].duration) {
-                    // Max speed
-                    tmp = TrackUtils.filterArray(featureMetrics, {
-                        speed: speed => speed !== 0 && speed !== undefined,
-                    })
-                    global.maxSpeed = Math.max(...tmp.map(a => a?.speed))
-
-                    // Average speed (we exclude 0 and undefined values)
-                    global.averageSpeed = tmp.reduce((s, o) => {
-                        return s + o.speed
-                    }, 0) / tmp.length
-
-                    // Todo  Add average speed in motion
-
-                    // Max Pace
-                    global.maxPace = Math.max(...featureMetrics.map(a => a?.pace))
-
-                    // Todo  Add average pace in motion
-                }
-
-                // Max Slope
-                global.maxSlope = Math.max(...featureMetrics.map(a => a?.slope))
-
-                // Positive elevation
-                global.positiveElevation = featureMetrics.reduce((s, o) => {
-                    if (o.elevation > 0) {
-                        return s + o.elevation
-                    }
-                    return s
-                }, 0)
-
-                // Negative elevation
-                global.negativeElevation = featureMetrics.reduce((s, o) => {
-                    if (o.elevation < 0) {
-                        return s + o.elevation
-                    }
-                    return s
-                }, 0)
-
-                // Total duration
-                global.duration = featureMetrics.reduce((s, o) => {
-                    return s + o.duration
-                }, 0)
-
-                // Total Distance
-                global.distance = featureMetrics.reduce((s, o) => {
-                    return s + o.distance
-                }, 0)
-
-                metrics.push({points: featureMetrics, global: global})
-            })
-            return metrics
-        })
-    }
-
 
 }
