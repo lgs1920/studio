a22c7b29f339f3bcb34edd3ee76bac694d37a502
chdenat <christian.denat@orange.fr>
2024-02-20 10:33:51 +0100

    - error instead of log

--- src/Components/UI/DropZoneUI/DropZoneUI.jsx
+++ src/Components/UI/DropZoneUI/DropZoneUI.jsx
@@ -6,12 +6,11 @@ export function DropZoneUI(props) {
         acceptedFiles.forEach((file) => {
             const reader = new FileReader()
 
-            reader.onabort = () => console.log('file reading was aborted')
-            reader.onerror = () => console.log('file reading has failed')
+            reader.onabort = () => console.error('file reading was aborted')
+            reader.onerror = () => console.error('file reading has failed')
             reader.onload = () => {
                 // Do whatever you want with the file contents
                 const binaryStr = reader.result
-                console.log(binaryStr)
             }
             reader.readAsArrayBuffer(file)
         })
