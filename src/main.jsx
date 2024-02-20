900ab408a894e5e9786aba721f09716031ea71fe
chdenat <christian.denat@orange.fr>
2024-02-20 19:23:42 +0100

    - add strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import {createRoot} from 'react-dom/client'
-import {ViewTrack3D} from './ViewTrack3D.jsx'
+import {StrictMode}  from 'react'
 
 import './assets/css/theme.css'
+import {createRoot}  from 'react-dom/client'
+import {ViewTrack3D} from './ViewTrack3D.jsx'
 import './assets/css/app.css'
 
-createRoot(document.getElementById('root')).render(
-    <ViewTrack3D />
-)
+createRoot(document.getElementById('root')).render(<StrictMode>
+    <ViewTrack3D/>
+</StrictMode>)
