694c74ff6ecfc47b8eb05ca2357467ac4505c26e
chdenat <christian.denat@orange.fr>
2024-02-24 16:01:36 +0100

    - add comments - conditional DEM server

--- src/Components/UI/TracksEditor/TrackSettings.jsx
+++ src/Components/UI/TracksEditor/TrackSettings.jsx
@@ -16,39 +16,69 @@ export const TrackSettings = forwardRef(function TrackSettings(props, ref) {
     const snapEditor = useSnapshot(storeEditor)
 
 
+    /**
+     * Change track Color
+     *
+     * @type {setColor}
+     */
     const setColor = (event => {
         storeEditor.track.color = event.target.value
         vt3d.addTrack(Object.assign({}, storeEditor.track))
         TracksEditorUtils.reRenderTrackSettings()
     })
 
+    /**
+     * Change Track Title
+     *
+     * The selection box is then synchronised
+     *
+     * @type {setTitle}
+     */
     const setTitle = (event => {
         storeEditor.track.title = event.target.value
         TracksEditorUtils.reRenderTracksList()
         vt3d.addTrack(Object.assign({}, storeEditor.track))
     })
 
+    /**
+     * Change track thickness
+     *
+     * @type {setThickness}
+     */
     const setThickness = (event => {
         storeEditor.track.thickness = event.target.value
         TracksEditorUtils.reRenderTrackSettings()
         vt3d.addTrack(Object.assign({}, storeEditor.track))
     })
 
+    /**
+     * CHange DEM server
+     *
+     * @type {setDEMServer}
+     */
     const setDEMServer = (event => {
         storeEditor.track.DEMServer = event.target.value
         TracksEditorUtils.reRenderTrackSettings()
-        vt3d.addTrack(Object.assign({}, storeEditor.track))
+        const tmp = Object.assign({}, storeEditor.track)
+        tmp.computeAll()
+        console.log(tmp)
+        vt3d.addTrack(tmp)
     })
 
     return (<>
         <div id="track-settings" key={vt3d.mainProxy.components.tracksEditor.trackSettingsKey}>
+            {/* Change visible name (title) */}
             <SlInput label="Name:" value={snapEditor.track.title}
                      onSlChange={setTitle}/>
-            <DEMServerSelection
-                default={snapEditor.track?.DEMServer ?? NO_DEM_SERVER}
-                label={'Simulate Altitude:'}
-                onChange={setDEMServer}
-            />
+
+            {/* Add DEM server selectionif we do ot have height initially (ie in the file) */
+                !snapEditor.track.hasHeight && <DEMServerSelection
+                    default={snapEditor.track?.DEMServer ?? NO_DEM_SERVER}
+                    label={'Simulate Altitude:'}
+                    onChange={setDEMServer}
+                />}
+
+            {/* Track line settings */}
             <label><SlIcon library="fa" name={FA2SL.set(faPenPaintbrush)}></SlIcon>Line</label>
             <br/>
             <div id="track-line-settings">
