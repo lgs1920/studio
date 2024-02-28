4240e85ab31910df09d8b65c31e12e5136e609d0
chdenat <christian.denat@orange.fr>
2024-02-28 15:13:26 +0100

    - files restructuration

--- src/Components/UI/VT3D_UI/VT3D_UI.jsx
+++ src/Components/UI/VT3D_UI/VT3D_UI.jsx
@@ -1,11 +1,11 @@
 import './style.css'
-import {forwardRef}        from 'react'
-import {useCesium}         from 'resium'
-import {CameraPositionUI}  from '../CameraPositionUI/CameraPositionUI'
-import {CompassUI}         from '../CompassUI/CompassUI'
-import {CreditsUI}         from '../CreditsUI/CreditsUI'
-import {TrackFileLoaderUI} from '../TrackFileLoaderUI/TrackFileLoaderUI'
-import {TracksEditor}      from '../TracksEditor/TracksEditor'
+import { forwardRef }        from 'react'
+import { useCesium }         from 'resium'
+import { CameraPositionUI }  from '../../cesium/CameraPositionUI/CameraPositionUI'
+import { CompassUI }         from '../../cesium/CompassUI/CompassUI'
+import { CreditsUI }         from '../CreditsUI/CreditsUI'
+import { TrackFileLoaderUI } from '../TrackFileLoaderUI/TrackFileLoaderUI'
+import { TracksEditor }      from '../TracksEditor/TracksEditor'
 
 export const VT3D_UI = forwardRef(function VT3D_UI(props, ref) {
     vt3d.viewer = useCesium().viewer
