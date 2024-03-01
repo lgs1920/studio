0b95e2e951224316b5b71e47d68561c42dce0e9e
chdenat <christian.denat@orange.fr>
2024-03-01 19:48:51 +0100

    - use MarkerUtils

--- src/classes/MapMarker.js
+++ src/classes/MapMarker.js
@@ -1,5 +1,4 @@
-import { icon, library } from '@fortawesome/fontawesome-svg-core'
-import * as Cesium       from 'cesium'
+import { MarkerUtils } from '../Utils/cesium/MarkerUtils'
 
 
 // Pin Marker Type
@@ -35,17 +34,17 @@ export class MapMarker {
     #image
 
     constructor(options) {
-        this.#type = options.type
-        this.#name = options.name
-        this.#coordinates = options.coordinates || {}
-        this.#backgroundColor = options.backgroundColor
-        this.#foregroundColor = options.foregroundColor
-        this.#border = options.border ?? 0
-        this.#text = options.text ?? undefined
-        this.#icon = options.icon ?? undefined
-        this.#size = options.size ?? (this.#type === MARKER_CIRCLE ? 10 : 48)
-        this.#description = options.description ?? undefined
-        this.#image = options.image ?? undefined
+        this.type = options.type
+        this.name = options.name
+        this.coordinates = options.coordinates || {}
+        this.backgroundColor = options.backgroundColor
+        this.foregroundColor = options.foregroundColor
+        this.border = options.border ?? 0
+        this.text = options.text ?? undefined
+        this.icon = options.icon ?? undefined
+        this.size = options.size ?? (this.type === MARKER_CIRCLE ? 10 : 48)
+        this.description = options.description ?? undefined
+        this.image = options.image ?? undefined
 
         this.register()
     }
@@ -71,61 +70,9 @@ export class MapMarker {
     }
 
     draw = () => {
-
-        let markerOptions = {
-            name: this.#name,
-            description: this.#description,
-            position: Cesium.Cartesian3.fromDegrees(this.#coordinates[0], this.#coordinates[1], this.#coordinates[2]),
-        }
-        const pinBuilder = new Cesium.PinBuilder()
-        if ([MARKER_COLOR, MARKER_COLOR, MARKER_TEXT].includes(this.#type)) {
-            markerOptions.billboard = {
-                heightReference: Cesium.HeightReference.CLAMP_TO_GROUND,
-                verticalOrigin: Cesium.VerticalOrigin.BOTTOM,
-            }
-        } else {
-
-        }
-
-        const backgroundColor = Cesium.Color.fromCssColorString(this.#backgroundColor)
-        const foregroundColor = this.#foregroundColor ? Cesium.Color.fromCssColorString(this.#foregroundColor) : undefined
-
-        switch (this.#type) {
-            case MARKER_CIRCLE:
-                this.#marker = vt3d.viewer.entities.add({
-                    point: {
-                        position: Cesium.Cartesian3.fromDegrees(this.#coordinates[0], this.#coordinates[1]),
-                        backgroundPadding: new Cesium.Cartesian2(8, 4),
-                        heightReference: Cesium.HeightReference.CLAMP_TO_GROUND,
-                        disableDepthTestDistance: Number.POSITIVE_INFINITY,
-                        color: this.#backgroundColor,
-                        pixelSize: 50,//this.#size,
-                        outlineColor: this.#foregroundColor,
-                        outlineWidth: this.#border,
-                    },
-                })
-                break
-            case MARKER_COLOR:
-                markerOptions.billboard.image = pinBuilder.fromColor(backgroundColor, this.#size).toDataURL()
-                this.#marker = vt3d.viewer.entities.add(markerOptions)
-                break
-            case MARKER_TEXT:
-                markerOptions.billboard.image = pinBuilder.fromText(this.#text, backgroundColor, this.#size).toDataURL()
-                this.#marker = vt3d.viewer.entities.add(markerOptions)
-                break
-            case MARKER_ICON:
-
-                markerOptions.billboard.image = pinBuilder.fromUrl(this.useFontAwesome(this.#icon), backgroundColor, this.#size)
-                this.#marker = vt3d.viewer.entities.add(markerOptions)
-                break
-        }
-
-        this.id = this.#marker.id
+        this.marker = MarkerUtils.draw(this)
+        this.id = this.marker.id
 
     }
 
-    useFontAwesome = (iconDefinition) => {
-        library.add(iconDefinition)
-        return `data:image/svg+xml,${encodeURIComponent(icon(iconDefinition).html)}`
-    }
 }
