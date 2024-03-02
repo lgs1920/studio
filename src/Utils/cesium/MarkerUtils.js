0587542e68195ac715eab93a66830f0d89527062
chdenat <christian.denat@orange.fr>
2024-03-02 16:01:35 +0100

    - for backup

--- src/Utils/cesium/MarkerUtils.js
+++ src/Utils/cesium/MarkerUtils.js
@@ -2,14 +2,15 @@ import { icon, library } from '@fortawesome/fontawesome-svg-core'
 import * as Cesium       from 'cesium'
 
 // Pin Marker Type
-export const MARKER_ICON = 1
-export const MARKER_TEXT = 2
-export const MARKER_COLOR = 3
+export const PIN_ICON = 1
+export const PIN_TEXT = 2
+export const PIN_COLOR = 3
 //Other paths
-export const MARKER_CIRCLE = 4
+export const PIN_CIRCLE = 4
+export const JUST_ICON = 5
 
 export class MarkerUtils {
-    static draw = (marker) => {
+    static draw = async (marker) => {
 
         let markerOptions = {
             name: marker.name,
@@ -29,7 +30,7 @@ export class MarkerUtils {
         let entity
 
         switch (marker.type) {
-            case MARKER_CIRCLE:
+            case PIN_CIRCLE:
                 entity = vt3d.viewer.entities.add({
                     point: {
                         position: Cesium.Cartesian3.fromDegrees(marker.coordinates[0], marker.coordinates[1]),
@@ -43,25 +44,85 @@ export class MarkerUtils {
                     },
                 })
                 break
-            case MARKER_COLOR:
+            case PIN_COLOR:
                 markerOptions.billboard.image = pinBuilder.fromColor(backgroundColor, marker.size).toDataURL()
-                entity = vt3d.viewer.entities.add(markerOptions)
+                entity = await vt3d.viewer.entities.add(markerOptions)
                 break
-            case MARKER_TEXT:
+            case PIN_TEXT:
                 markerOptions.billboard.image = pinBuilder.fromText(marker.text, backgroundColor, marker.size).toDataURL()
-                entity = vt3d.viewer.entities.add(markerOptions)
+                entity = await vt3d.viewer.entities.add(markerOptions)
                 break
-            case MARKER_ICON:
-                markerOptions.billboard.image = pinBuilder.fromUrl(MarkerUtils.useFontAwesome(marker.icon), backgroundColor, marker.size)
-                entity = vt3d.viewer.entities.add(markerOptions)
+            case PIN_ICON:
+                pinBuilder.fromUrl(MarkerUtils.useFontAwesome(marker).src, backgroundColor, marker.size).then(async image => {
+                    markerOptions.billboard.image = image
+                    entity = await vt3d.viewer.entities.add(markerOptions)
+                })
+                break
+            case JUST_ICON:
+                MarkerUtils.useOnlyFontAwesome(marker).then(async canvas => {
+                    markerOptions.billboard.image = canvas
+                    entity = await vt3d.viewer.entities.add(markerOptions)
+                })
                 break
         }
 
         return entity
-
     }
-    static useFontAwesome = (iconDefinition) => {
-        library.add(iconDefinition)
-        return `data:image/svg+xml,${encodeURIComponent(icon(iconDefinition).html)}`
+    static useFontAwesome = (marker) => {
+        library.add(marker.icon)
+        const html = icon(marker.icon).html[0]
+        // Get SVG
+        const svg = (new DOMParser()).parseFromString(html, 'image/svg+xml').querySelector('svg')
+        svg.querySelector('path').setAttribute('fill', 'yellow')
+        return {
+            src: `data:image/svg+xml,${encodeURIComponent(svg.outerHTML)}`,
+            width: svg.viewBox.baseVal.width,
+            height: svg.viewBox.baseVal.height,
+        }
+    }
+
+    static useOnlyFontAwesome = async (marker) => {
+
+        function loadElement(imageURL) {
+            return new Promise((resolve) => {
+                const image = new Image()
+                image.addEventListener('load', () => {
+                    resolve(image)
+                })
+                image.src = imageURL
+            })
+        }
+
+        const canvas = document.createElement('canvas')
+        const context = canvas.getContext('2d')
+        const image = MarkerUtils.useFontAwesome(marker)
+        const ratio = window.devicePixelRatio || 1
+        // context.scale(ratio, ratio)
+        context.imageSmoothingEnabled = true
+        context.imageSmoothingQuality = 'high'
+        canvas.width = image.width * ratio * marker.size / image.height
+        canvas.height = ratio * marker.size
+        const v = await canvg.from(context)
+
+        // 1. Multiply the canvas's width and height by the devicePixelRatio
+
+
+        // 2. Force it to display at the original (logical) size with CSS or style attributes
+        // canvas.style.width = marker.size + 'px'
+        // canvas.style.height = marker.size + 'px'
+
+        // 3. Scale the context so you can draw on it without considering the ratio.
+
+        return loadElement(image.src)
+            .then((image) => {
+                var canvasW = canvas.width, canvasH = canvas.height
+                var imgW = image.naturalWidth || canvasW, imgH = image.naturalHeight || canvasH
+
+                console.log(imgW, imgH, canvasW, canvasH)
+                context.drawImage(image, 0, 0, canvasW, imgH * canvasW / imgW)
+                return canvas
+            })
+
+
     }
 }
