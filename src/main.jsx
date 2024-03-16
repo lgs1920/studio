40dda317ee629ba971806fadbb36626a1dba9811
chdenat <christian.denat@orange.fr>
2024-03-16 19:13:09 +0100

    - step...

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import './assets/css/theme.css'
-import {createRoot}  from 'react-dom/client'
-import {ViewTrack3D} from './ViewTrack3D.jsx'
+import './assets/css/light.css'
+
+import { createRoot }  from 'react-dom/client'
+import { ViewTrack3D } from './ViewTrack3D.jsx'
 import './assets/css/app.css'
 
 
