c274dcb646f3af9aca16e29bd5941a9cb52bac05
chdenat <christian.denat@orange.fr>
2024-04-02 18:21:46 +0200

    - show track nearest point

--- src/Components/UI/FloatingMenu/TrackMenu.js
+++ src/Components/UI/FloatingMenu/TrackMenu.js
@@ -1,6 +1,8 @@
-import * as Cesium    from 'cesium'
-import { TRACK_TYPE } from '../../../Utils/cesium/EntitiesUtils'
-import { MouseUtils } from '../../../Utils/cesium/MouseUtils'
+import { point }                         from '@turf/helpers'
+import { default as nearestPointOnLine } from '@turf/nearest-point-on-line'
+import * as Cesium                       from 'cesium'
+import { TRACK_TYPE }                    from '../../../Utils/cesium/EntitiesUtils'
+import { MouseUtils }                    from '../../../Utils/cesium/MouseUtils'
 
 export class TrackMenu {
     static show = (data) => {
@@ -14,19 +16,36 @@ export class TrackMenu {
         // Save track in store
         menuStore.target = data.picked
 
+        // Get coordinates of clicked point
         const position = data.positions.position ?? data.positions.position.endPosition
         const cartesian = vt3d.viewer.camera.pickEllipsoid(position, vt3d.viewer.scene.globe.ellipsoid)
 
         if (cartesian) {
+            // Find the nearest point on the track
             const cartographic = Cesium.Cartographic.fromCartesian(cartesian)
-            menuStore.longitude = Cesium.Math.toDegrees(cartographic.longitude)
-            menuStore.latitude = Cesium.Math.toDegrees(cartographic.latitude)
+            const clickedPoint = point([Cesium.Math.toDegrees(cartographic.longitude), Cesium.Math.toDegrees(cartographic.latitude)])
+            const nearest = nearestPointOnLine(data.picked.track.geoJson.features[0].geometry, clickedPoint)
+            // We need coordinates
+            menuStore.longitude = nearest.geometry.coordinates[0]
+            menuStore.latitude = nearest.geometry.coordinates[1]
+            // But also the window coordinates and point inex
+            const {
+                      x,
+                      y,
+                  } = Cesium.SceneTransforms.wgs84ToWindowCoordinates(vt3d.viewer.scene, Cesium.Cartesian3.fromDegrees(menuStore.longitude, menuStore.latitude))
 
-            let {x, y} = Cesium.SceneTransforms.wgs84ToWindowCoordinates(vt3d.viewer.scene, cartesian)
             menuStore.coordinates.x = x
             menuStore.coordinates.y = y
+            menuStore.index = nearest.properties.index
+            data.picked.index = nearest.properties.index
 
+            // let's show the menu
             MouseUtils.showMenu(data.picked.type)
+
+            // we show clicked point on the track
+            vt3d.events.emit('wander/drawPoint', data)
+
+
         }
     }
 
