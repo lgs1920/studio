b9107a02384110694a11866d106cc57cbd96508b
chdenat <christian.denat@orange.fr>
2024-04-09 11:27:14 +0200

    - add deep clone

--- src/Utils/AppUtils.js
+++ src/Utils/AppUtils.js
@@ -109,6 +109,22 @@ export class AppUtils {
         return single
     }
 
+    static deepClone = obj => {
+        if (obj === null) return null
+        let clone = Object.assign({}, obj)
+        Object.keys(clone).forEach(
+            key =>
+                (clone[key] =
+                    typeof obj[key] === 'object' ? AppUtils.deepClone(obj[key]) : obj[key]),
+        )
+        if (Array.isArray(obj)) {
+            clone.length = obj.length
+            return Array.from(clone)
+        }
+        return clone
+    }
+
+
 }
 
 /** Time ans duration constants in seconds */
