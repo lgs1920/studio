c1ff85cf16db1bf99a1e14598704f20969642778
chdenat <christian.denat@orange.fr>
2024-04-21 15:06:22 +0200

    - Track settings based on journey visibility

--- src/Components/UI/TracksEditor/TracksEditor.jsx
+++ src/Components/UI/TracksEditor/TracksEditor.jsx
@@ -51,24 +51,25 @@ export const TracksEditor = forwardRef(function TracksEditor(props, ref) {
 
     return (<>
         <div id="journeys-editor-container" key={mainSnap.key}>
-            {mainSnap.usable &&
-                <SlDrawer id="journeys-editor-pane" open={mainSnap.show}
-                          onSlRequestClose={handleRequestClose}
-                          contained
-                          onSlHide={closeTracksEditor}
-                    // onSlShow={TracksEditorUtils.initJourneyEdition}
-                >
-                    {vt3d.journeys.size > 0 &&
-                        <div id={'track-settings-container'}>
-                            <JourneySelector onChange={TracksEditorUtils.initJourneyEdition}
-                                             label={'Select a Journey:'}/>
-                            <JourneySettings/>
-                            <TrackSelector onChange={TracksEditorUtils.initTrackEdition}
-                                           label={'Select one of the tracks:'}/>
-                            <TrackSettings/>
-                        </div>}
-                    <div id="journeys-editor-footer" slot={'footer'}></div>
-                </SlDrawer>}
+            {mainSnap.usable && <SlDrawer id="journeys-editor-pane" open={mainSnap.show}
+                                          onSlRequestClose={handleRequestClose}
+                                          contained
+                                          onSlHide={closeTracksEditor}
+                // onSlShow={TracksEditorUtils.initJourneyEdition}
+            >
+                {vt3d.journeys.size > 0 && <div id={'track-settings-container'}>
+                    <JourneySelector onChange={TracksEditorUtils.initJourneyEdition}
+                                     label={'Select a Journey:'}/>
+                    <JourneySettings/>
+
+                    {editorStore.journey.visible && <>
+                        <TrackSelector onChange={TracksEditorUtils.initTrackEdition}
+                                       label={'Select one of the tracks:'}/>
+                        <TrackSettings/>
+                    </>}
+                </div>}
+                <div id="journeys-editor-footer" slot={'footer'}></div>
+            </SlDrawer>}
         </div>
 
         <SlTooltip placement={'right'} content="Edit Tracks">
