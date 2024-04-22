2972eb2da55dd2765ac074638815b662a50b6e29
chdenat <christian.denat@orange.fr>
2024-04-22 18:00:20 +0200

    - fix an issue with initial state

--- src/Components/UI/ToggleStateIcon.jsx
+++ src/Components/UI/ToggleStateIcon.jsx
@@ -1,31 +1,38 @@
-import { faEye, faEyeSlash } from '@fortawesome/pro-regular-svg-icons'
-import { SlIcon }            from '@shoelace-style/shoelace/dist/react'
-import React, { useState }   from 'react'
-import { FA2SL }             from '../../Utils/FA2SL'
+import { faEye, faEyeSlash }          from '@fortawesome/pro-regular-svg-icons'
+import { SlIcon }                     from '@shoelace-style/shoelace/dist/react'
+import React, { useEffect, useState } from 'react'
+import { FA2SL }                      from '../../Utils/FA2SL'
 
 export const ToggleStateIcon = (props) => {
 
     const change = props.change
-    const initial = props.initial ?? true
+    const initialState = props.initial ?? true
     const icons = props.icons ?? {shown: faEye, hidden: faEyeSlash}
     const id = props.id ?? ''
     const style = props.style ?? ''
 
-    const [visibility, setVisibility] = useState(initial)
-    const toggleVisibility = () => {
-        setVisibility(!visibility)
-        change(!visibility)
+    const [state, setState] = useState(initialState)
+
+    const toggleState = async () => {
+        setState(!state)
+        change(!state)
     }
 
+    useEffect(() => {
+        setState(initialState)
+    }, [initialState])
+
     return (
-        <a className="visibility-button"
-           {...(props.id && {id})}
-           {...(props.style && {style})}
-           onClick={toggleVisibility}>
-            {visibility
-             ? <SlIcon slot="suffix" library="fa" name={FA2SL.set(icons.hidden)}/>
-             : <SlIcon slot="suffix" library="fa" name={FA2SL.set(icons.shown)}/>
-            }
-        </a>
+        <>
+            <a className="state-button"
+               {...(props.id && {id})}
+               {...(props.style && {style})}
+               onClick={toggleState}>
+                {state
+                 ? <SlIcon slot="suffix" library="fa" name={FA2SL.set(icons.hidden)}/>
+                 : <SlIcon slot="suffix" library="fa" name={FA2SL.set(icons.shown)}/>
+                }
+            </a>
+        </>
     )
 }
