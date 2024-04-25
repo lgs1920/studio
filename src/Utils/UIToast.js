b84e730a537eee03c0b9953a17b18d1d87e16a1f
chdenat <christian.denat@orange.fr>
2024-04-25 10:24:37 +0200

    - add toast animations

--- src/Utils/UIToast.js
+++ src/Utils/UIToast.js
@@ -1,8 +1,11 @@
 import {
     faCircleCheck, faCircleInfo, faHexagonExclamation, faTriangleExclamation,
-}                 from '@fortawesome/pro-regular-svg-icons'
-import { SECOND } from './AppUtils'
-import { FA2SL }  from './FA2SL'
+}                       from '@fortawesome/pro-regular-svg-icons'
+import { slideInUp }    from '@shoelace-style/animations/dist/sliding_entrances/slideInUp'
+import { slideOutLeft } from '@shoelace-style/animations/dist/sliding_exits/slideOutLeft'
+import { setAnimation } from '@shoelace-style/shoelace/dist/utilities/animation-registry'
+import { SECOND }       from './AppUtils'
+import { FA2SL }        from './FA2SL'
 
 export const VT3D_INFORMATION_TOAST = 'information'
 export const VT3D_SUCCESS_TOAST = 'success'
@@ -36,6 +39,20 @@ export class UIToast {
       `,
         })
 
+        // Add animations
+        setAnimation(alert, 'alert.hide', {
+            keyframes: slideOutLeft,
+            options: {
+                duration: 200,
+            },
+        })
+        setAnimation(alert, 'alert.show', {
+            keyframes: slideInUp,
+            options: {
+                duration: 400,
+            },
+        })
+
         document.body.append(alert)
         return alert.toast()
     }
@@ -43,13 +60,13 @@ export class UIToast {
     static notify = (message, duration = UIToast.DURATION) => {
         UIToast.#notify(message, VT3D_INFORMATION_TOAST, duration)
     }
-    static notifySuccess = (message, duration = UIToast.DURATION) => {
+    static success = (message, duration = UIToast.DURATION) => {
         UIToast.#notify(message, VT3D_SUCCESS_TOAST, duration)
     }
-    static notifyWarning = (message, duration = UIToast.DURATION) => {
+    static warning = (message, duration = UIToast.DURATION) => {
         UIToast.#notify(message, VT3D_WARNING_TOAST, duration)
     }
-    static notifyError = (message, duration = UIToast.DURATION) => {
+    static error = (message, duration = UIToast.DURATION) => {
         UIToast.#notify(message, VT3D_ERROR_TOAST, duration)
     }
 
