9704e813c8d58dad72830b18d5dee8138ee587a2
chdenat <christian.denat@orange.fr>
2024-05-25 19:04:38 +0200

    - better add management and getter

--- src/core/ui/settings/Settings.js
+++ src/core/ui/settings/Settings.js
@@ -1,20 +1,32 @@
-import { SettingsStore } from './SettingsStore.js'
-
 export class Settings {
 
+    /** @type {Map} */
+    #sections
+
     constructor() {
+
         if (Settings.instance) {
-        return Settings.instance
+            return Settings.instance
+        }
+
+        this.#sections = new Map()
+        Settings.instance= this
     }
 
-        // App store
-        this.app =  new SettingsStore('app',{
-            firstVisit:false,
-            lastVisit:null
-        })
+    /**
+     * Add a new section to Settings and define getter using the key
+     *
+     * @param {string} key
+     * @param {object} section
+     */
+    add = (key, section) => {
+        this.#sections.set(key, section)
+        Object.defineProperty(this, key, {
+                get: function () {
+                    return this.#sections.get(key).content
+                },
+            })
 
 
-
-        Settings.instance= this
     }
 }
---
+++ src/core/ui/settings/SettingsSection.js
@@ -0,0 +1,72 @@
+import { proxy, subscribe } from 'valtio'
+import { SETTINGS_STORE }   from '../../LGS1920Context.js'
+
+export class SettingsSection {
+
+    /** @type {string} */
+    key
+    /** @type {object} */
+    #content
+
+    /**
+     *
+     * @param {string} key
+     * @param {object} section
+     */
+    constructor(key, section) {
+        this.key = key
+        this.#content = proxy(section)
+        let data = {};
+
+        (async () => {
+            data = await this.read()
+            if (data === null) {
+                (async () => {
+                    await this.save()
+                })()
+            } else {
+                this.#content = proxy(data)
+            }
+        })()
+
+
+        // Each time we update a parameter in this store, we save it
+        subscribe(this.#content, () => {
+            console.log('subscribe')
+            this.save()
+        })
+
+
+    }
+
+    /**
+     * Content getter
+     *
+     * @return {Proxy}
+     */
+    get content() {
+        return this.#content
+    }
+
+    /**
+     * Save settings in indexedDB: Settings object is unproxified before saving
+     *
+     * @return {Promise<void>}
+     */
+    save = async () => {
+        console.log('save')
+        await lgs.db.lgs1920.put(this.key, JSON.parse(JSON.stringify(this.#content)), SETTINGS_STORE)
+    }
+
+    /**
+     *  Read Settings: We proxify the read object
+     *
+     *  @param  {any} parameter : if specified we return only the corresponding value
+     *
+     *  @return {Promise<void>}
+     */
+    read = async (parameter = undefined) => {
+        const all = await lgs.db.lgs1920.get(this.key, SETTINGS_STORE)
+        return parameter ? all[parameter] ?? undefined : all
+    }
+}
