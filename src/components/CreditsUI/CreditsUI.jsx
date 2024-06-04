a0a8d08565263f59803115f18492fdd6299ca5d9
chdenat <christian.denat@orange.fr>
2024-06-04 17:34:47 +0200

    - enhancements

--- src/components/CreditsUI/CreditsUI.jsx
+++ src/components/CreditsUI/CreditsUI.jsx
@@ -1,44 +1,51 @@
-//read version
-import { faGithub }                                                       from '@fortawesome/free-brands-svg-icons'
-import { faCircleInfo }                                                   from '@fortawesome/pro-regular-svg-icons'
-import { SlButton, SlDrawer, SlIcon, SlIconButton, SlInclude, SlTooltip } from '@shoelace-style/shoelace/dist/react'
-import { FA2SL }                                                          from '@Utils/FA2SL'
-import { forwardRef }                                                     from 'react'
-import { useSnapshot }                                                    from 'valtio'
+import { DrawerFooter }                          from '@Components/DrawerFooter'
+import { faCircleInfo }                          from '@fortawesome/pro-regular-svg-icons'
+import { SlButton, SlDrawer, SlIcon, SlTooltip } from '@shoelace-style/shoelace/dist/react'
+import { FA2SL }                                 from '@Utils/FA2SL'
+import React, { useEffect, useRef }              from 'react'
+import { Scrollbars }                            from 'react-custom-scrollbars'
+import { default as ReactMarkdown }              from 'react-markdown'
+import { useSnapshot }                           from 'valtio'
+import engine                                    from '../../../public/assets/credits/credits-engine.md'
+import providers                                 from '../../../public/assets/credits/credits-map-providers.md'
+import code                                      from '../../../public/assets/credits/credits-open-source-code.md'
 
-
-import './style.css'
-
-export const CreditsUI = forwardRef(function CreditsUI() {
+export const CreditsUI = function CreditsUI() {
 
     const mainSnap = useSnapshot(lgs.mainUIStore)
+    const drawerRef = useRef(null)
 
     const setOpen = (open) => {
         lgs.mainUIStore.credits.show = open
     }
 
-    const version = async()=> {
-        return  await fetch('/version.json').then(
-            res => res.json()
-        )
-    }
+        useEffect(() => {
+            //search the link and add external target (as it is not possible in markdown)
+            if (drawerRef.current) {
+                const slotBody = drawerRef.current.shadowRoot.querySelector('slot[part="body"]')
+                const assignedElements = slotBody.assignedElements()
+                assignedElements[0].querySelectorAll('a').forEach(link => {
+                    link.target = '_blank'
+                })
+            }
+
+        }, [])
 
     return (<>
-        <SlDrawer className="ui-element- transparent" id="credits-pane" open={mainSnap.credits.show}
-                  onSlAfterHide={() => setOpen(false)}>
-            <SlInclude src="/src/assets/pages/credits.html"/>
+        <SlDrawer className="ui-element- transparent" id="credits-pane"
+                  open={mainSnap.credits.show}
+                  onSlAfterHide={() => setOpen(false)}
+                  ref={drawerRef}
+                  >
 
-            <div id="credits-pane-footer" slot={'footer'}>
-                <div>
-                    <strong>{lgs.configuration.applicationName}</strong><span>{version().studio}</span>
-                </div>
-                <SlTooltip content="Our GitHub repo">
-                    <SlIconButton library="fa" name={FA2SL.set(faGithub)}
-                                  target={'_blank'}
-                                  href={'https://github.com/ViewTrack3D/lgs'}
-                    />
-                </SlTooltip>
-            </div>
+            <Scrollbars>
+                <ReactMarkdown children={engine}/>
+                <ReactMarkdown children={providers}/>
+                <ReactMarkdown children={code}/>
+            </Scrollbars>
+
+            <DrawerFooter/>
+
         </SlDrawer>
         <SlTooltip placement={'right'} content="Show Credits">
             <SlButton className={'square-icon'} size="small" id={'open-credits-pane'} onClick={() => setOpen(true)}>
@@ -47,4 +54,4 @@ export const CreditsUI = forwardRef(function CreditsUI() {
         </SlTooltip>
 
     </>)
-})
+    }
