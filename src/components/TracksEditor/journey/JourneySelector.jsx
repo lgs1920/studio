7b3998d6725ff4fdcdaec9e34831f4db7dc9b4f7
chdenat <christian.denat@orange.fr>
2024-08-06 09:18:07 +0200

    - code

--- src/components/TracksEditor/journey/JourneySelector.jsx
+++ src/components/TracksEditor/journey/JourneySelector.jsx
@@ -2,14 +2,9 @@ import { faChevronDown, faEye, faEyeSlash } from '@fortawesome/pro-regular-svg-i
 import { faRoute, faSquare }                from '@fortawesome/pro-solid-svg-icons'
 import { SlIcon, SlOption, SlSelect }       from '@shoelace-style/shoelace/dist/react'
 import { FA2SL }                            from '@Utils/FA2SL'
-import { forwardRef }                       from 'react'
 import { useSnapshot }                      from 'valtio'
 
-export const JourneySelector = forwardRef(function JourneySelector(props, ref) {
-
-    const handleRequestClose = event => {
-        event.preventDefault()
-    }
+export const JourneySelector = (props) => {
     const mainStore = lgs.mainProxy.components.journeyEditor
     const mainSnapshot = useSnapshot(mainStore)
     const editorStore = lgs.theJourneyEditorProxy
@@ -96,4 +91,4 @@ export const JourneySelector = forwardRef(function JourneySelector(props, ref) {
         </>}
 
     </>)
-})
+}
