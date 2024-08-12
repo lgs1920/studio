5a8978469c547309619d06f1dcb00fa0f957e304
chdenat <christian.denat@orange.fr>
2024-08-12 12:07:05 +0200

    - update Journey Loader Icon

--- src/components/MainUI/WelcomeModal.jsx
+++ src/components/MainUI/WelcomeModal.jsx
@@ -9,6 +9,7 @@ import welcome     from '../../../public/assets/modals/welcome.md'
 import welcomeBack from '../../../public/assets/modals/welcome-back.md'
 
 
+import {faRegularRouteCirclePlus}              from '@awesome.me/kit-eb5c406148/icons/kit/custom'
 
 
 export const WelcomeModal = () => {
@@ -92,7 +93,7 @@ export const WelcomeModal = () => {
                             }
                             {editorPanel.list.length === 0 &&
                                 <SlButton autofocus variant="primary" onClick={loadJourney}>
-                                    <SlIcon slot="prefix" library="fa" name={FA2SL.set(faRoute)}></SlIcon>{'Load your first Journey'}
+                                    <SlIcon slot="prefix" library="fa" name={FA2SL.set(faRegularRouteCirclePlus)}></SlIcon>{'Load your first Journey'}
                                 </SlButton>
                             }
                         </div>
