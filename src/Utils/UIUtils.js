d30465ff199e2ff30015b68d4a10d644ddfb0ee7
chdenat <christian.denat@orange.fr>
2024-09-01 19:47:33 +0200

    add rgb utils

--- src/Utils/UIUtils.js
+++ src/Utils/UIUtils.js
@@ -13,6 +13,50 @@ export class UIUtils {
         div.textContent = html
         return div.innerHTML
     })
+
+    /**
+     * Transform a color in hexa to rgb() or rgba()
+     *
+     * @param hex {string}  #RRGGBBAA,#RGB ou #RRGGBB
+     * @param format        output format (rgb | rgba), default rgba
+     * @return {string}       rgb() or rgba()
+     */
+    static hexToRGBA=(hex, format = 'rgba')=> {
+        hex = hex.replace(/^#/, '');
+
+        // Transform #RGB to #RRGGBB orRRRRRGBFF
+        if (hex.length === 3) {
+            hex = hex.split('').map(char => char + char).join('');
+            if (format === 'rgba') {
+                hex +='FF'
+            }
+        }
+
+        // Extract colors
+        let bigint = parseInt(hex, 16);
+        let r = (bigint >> 24) & 255;
+        let g = (bigint >> 16) & 255;
+        let b = (bigint >> 8) & 255;
+
+        if (format === 'rgb') {
+            return `rgb(${r},${g},${b})`;
+        }
+        // and alpha,if it exists
+        if (hex.length === 8) {
+            let a = (bigint & 255) / 255;
+            return `rgba(${r},${g},${b},${a})`;
+        }
+    }
+
+
+    static RGB2RGBA=(rgbString, alpha = 1) => {
+        let rgbValues = rgbString.match(/\d+/g);
+        let r = rgbValues[0];
+        let g = rgbValues[1];
+        let b = rgbValues[2];
+        return `rgba(${r},${g},${b},${alpha})`;
+    }
+
 }
 
 
