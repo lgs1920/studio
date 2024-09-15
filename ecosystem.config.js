4f83f28e09476025460fb099e9350ca79c8b0673
chdenat <christian.denat@orange.fr>
2024-09-15 11:50:50 +0200

    deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
       "host" : '91.207.254.129',
       "ref"  : "origin/main",
       "repo" : "git@github.com:lgs1920/studio.git",
-      "path" : "/home/christian/devs/assets/lgs1920/studio",
+      "path" : "/home/www/lgs1920/studio",
       "post-deploy" : "bun install"
     }
   }
