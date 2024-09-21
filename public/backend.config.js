038d4b723b8de5cb2f2edb611ab54b0147799363
chdenat <christian.denat@orange.fr>
2024-09-21 07:59:42 +0200

    remove

--- public/backend.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    apps : [{
-        name   : "backend",
-        script : "/home/.bun/bin/bun run ../../backend/dist/index.js",
-        watch : true,
-        wait_ready: true
-    }]
-}
