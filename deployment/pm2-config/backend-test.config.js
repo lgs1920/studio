6f5389a2efbf0b78e904af6e921a6c55649c79c4
chdenat <christian.denat@orange.fr>
2024-09-21 07:59:31 +0200

    new deployment strategy

--- deployment/pm2-config/backend-test.config.js
+++ deployment/pm2-config/backend-test.config.js
@@ -4,8 +4,8 @@
  * This file is part of the LGS1920/backend project.                                                                  *
  *                                                                                                                    *
  *                                                                                                                    *
- * File: deploy.js                                                                                                    *
- * Path: /home/christian/devs/assets/lgs1920/backend/deploy.js                                                        *
+ * File: backend-test.config.js                                                                                       *
+ * Path: /home/christian/devs/assets/lgs1920/backend/deployment/pm2-config/backend-test.config.js                     *
  *                                                                                                                    *
  * Author : Christian Denat                                                                                           *
  * email: christian.denat@orange.fr                                                                                   *
@@ -18,44 +18,11 @@
  *                                                                                                                    *
  **********************************************************************************************************************/
 
-import argparse     from 'argparse'
-import path         from 'path'
-import {Deployment} from './deployment/Deployment.js'
-
-const fs = require('fs')
-const yaml = require('yaml')
-
-const platforms = {production: 'production', staging: 'staging', test: 'test'}
-
-/*******************************************************************************
- * Read/manage arguments
- */
-const parser = new argparse.ArgumentParser(
-    {
-        description: 'LGS1920 products deployment script',
-        usage:       'deploy --prod|-p, --staging|-s, --test|-t or --help|-h',
-    },
-)
-
-parser.add_argument('--prod', '-p', {
-    action: 'store_true',
-    help:   'Deploy to production platform',
-})
-
-parser.add_argument('--staging', '-s', {
-    action: 'store_true',
-    help:   'Deploy to staging platform',
-})
-
-parser.add_argument('--test', '-t', {
-    action: 'store_true',
-    help:   'Deploy to test platform',
-})
-const args = parser.parse_args()
-
-new Deployment(
-    {
-        local:   path.dirname(__dirname),
-        platform:args.prod ? platforms.production : args.staging ? platforms.staging : platforms.test,
-        product:import.meta.env.LGS1920_PRODUCT
-    })
+module.exports = {
+    apps : [{
+        name   : "backend-test",
+        script : "/home/.bun/bin/bun run backend.js",
+        watch : true,
+        wait_ready: true
+    }]
+}
