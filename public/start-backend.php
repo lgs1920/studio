6c5589b1b0dd3e7c4f9a5b9652b7685d37405347
chdenat <christian.denat@orange.fr>
2024-09-21 17:11:38 +0200

    - reflect deployment change

--- public/start-backend.php
+++ public/start-backend.php
@@ -1,23 +1,29 @@
 <?php
+function checkAlive($bin, $platform) {
+    $list = json_decode(shell_exec(sprintf('%s jlist', $bin)), true);
+    foreach ($list as $item) {
+        if ($item['name'] === "backend-$platform" && $item['pm2_env']['status'] === 'online') {
+            return true;
+        }
+    }
+    return false;
+}
+
+
 /**
  * Run backend command
  */
-$config = json_decode(file_get_contents('./config.json'), true);
-// Start backend
-shell_exec(sprintf('%s %s',$config['backend']['pm2'],$config['backend']['command'])) ;
-// Now verify if it is online
-$list = json_decode(shell_exec(sprintf('%s jlist',$config['backend']['pm2'])),true) ;
+$config = json_decode(file_get_contents('servers.json'), true);
 
+$response=['alive'=> false];
 
-// check if backend is active
-$response = array('alive' => false);
-foreach ($list as $item) {
-    if ($item['name'] === 'backend' && $item['pm2_env']['status'] === 'online') {
-        $response['alive'] = true;
-        break;
-    }
+if (checkAlive($config['backend']['pm2']['bin'], $config['platform'])) {
+    $response['alive'] = true;
+} else {
+    // Start backend
+    shell_exec($config['backend']['pm2']['command']);
+    $response['alive']=checkAlive($config['backend']['pm2']['bin'], $config['platform']);
 }
 
 // Return the status
-print json_encode($response);
-
+    print json_encode($response);
