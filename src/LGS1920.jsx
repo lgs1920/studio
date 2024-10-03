8b7ec9e332037c5ab03f0ddb667c061490126b87
chdenat <christian.denat@orange.fr>
2024-10-03 19:33:08 +0200

    enhancements

--- src/LGS1920.jsx
+++ src/LGS1920.jsx
@@ -4,7 +4,7 @@ import { LGS1920Context }                            from '@Core/LGS1920Context'
 import { CameraUtils }                               from '@Utils/cesium/CameraUtils'
 import { TrackUtils }                                from '@Utils/cesium/TrackUtils'
 import * as Cesium                                   from 'cesium'
-import { useEffect, useRef }                         from 'react'
+import { useEffect }                                 from 'react'
 import { Camera, CameraFlyTo, Globe, Scene, Viewer } from 'resium'
 import { MapLayer }                                  from './components/cesium/MapLayer'
 import { InitErrorMessage }                          from './components/InitErrorMessage'
@@ -33,18 +33,15 @@ const initApp = await __.app.init()
 
 export function LGS1920() {
 
-    const viewerRef = useRef(null)
-
-   // const mainUISnapshot = useSnapshot(lgs.mainUIStore)
-
     const starter = lgs.configuration.starter
     lgs.startCameraPoint = Cesium.Cartesian3.fromDegrees(starter.camera.longitude, starter.camera.latitude, starter.camera.height)
-
     lgs.cameraOrientation = {
         heading: Cesium.Math.toRadians(starter.camera.heading),
         pitch: Cesium.Math.toRadians(starter.camera.pitch),
         roll: Cesium.Math.toRadians(starter.camera.roll),
     }
+
+
     const cameraStore = lgs.mainProxy.components.camera
 
     const run360 = () => {
@@ -83,28 +80,33 @@ export function LGS1920() {
                 await TrackUtils.readAllFromDB()
             }
 
+
             // Set DefaultTheme
-        __.app.setTheme()
+            __.app.setTheme()
 
             // Set body class to manage css versus platform
             document.body.classList.add(lgs.platform)
 
-        // Read DB
+            // Read DB
             readAllFromDB().then(
                 // Let's instantiate some elements Managers
                 lgs.initManagers(),
             )
 
-        //Ready
-        UIToast.success({
-            caption: `Welcome on ${lgs.configuration.applicationName}!`,
-            text: 'We\'re ready to assist you !',
-        })
+            //Ready
+            UIToast.success({
+                                caption: `Welcome on ${lgs.configuration.applicationName}!`,
+                                text:    'We\'re ready to assist you !',
+                            })
             console.log(`LGS1920 ${lgs.versions.studio} has been loaded and is ready on ${lgs.platform} platform !`)
             console.log(`Connected to backend ${lgs.versions.backend}.`)
         }
         else {
             // Init was wrong, let'stop here
+            UIToast.error({
+                              caption: `LGS1920 was stopped due to errors!`,
+                              text:    `We're sorry`,
+                          })
             console.log('LGS1920 was stopped due to errors!')
             console.error(initApp.error)
         }
@@ -123,30 +125,30 @@ export function LGS1920() {
                 homeButton={false}
                 navigationHelpButton={false}
                 fullscreenButton={false}
-
+                geocoder={false}
+                infoBox={false}
                 sceneModePicker={false}
-
+                showRenderLoopErrors={false}
             //terrain={Cesium.Terrain.fromWorldTerrain({
 
                 terrain={new Cesium.Terrain(Cesium.CesiumTerrainProvider.fromUrl(`https://api.maptiler.com/tiles/terrain-quantized-mesh-v2/?key=${'Y6VgRYi3iKQEttoa3G0v'}`, {
                     requestVertexNormals: false,
                 }))}
 
-                id="viewTrack3DViewer"
+                id="studioMapViewer"
+
+            /***********************/
             // Avoid consuming Cesium Ion Sessions
-            // DONOT CHANGE
+            // DO NOT CHANGE the 2 following lines
                 imageryProvider={false}
                 baseLayerPicker={false}
-                ref={viewerRef}>
+            /***********************/
+
+        >
             <MapLayer/>
 
-            <Scene verticalExaggeration={1.25}
-                   verticalExaggerationRelativeHeight={3000.0}>
-            </Scene>
-            <Globe enableLighting={false}
-                   depthTestAgainstTerrain ={true}
-            >
-            </Globe>
+            <Scene verticalExaggeration={1.25}></Scene>
+            <Globe enableLighting={false} depthTestAgainstTerrain={true}></Globe>
             <Camera>
                 <CameraFlyTo
                     orientation={lgs.cameraOrientation}
