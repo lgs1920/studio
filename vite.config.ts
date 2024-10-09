5bbe4d9775f83579a8f715f14e10eebd46bf05b0
chdenat <christian.denat@orange.fr>
2024-10-09 07:10:46 +0200

    code

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-import {defineConfig,loadEnv} from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import cesium from 'vite-plugin-cesium';
 
-import data from './public/version.json' with { type: "json" };
+import data from './public/version.json' with {type: 'json'};
 
 const version =data.studio
 
