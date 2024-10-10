cf138c03923818b0a058c228d46561551868106b
chdenat <christian.denat@orange.fr>
2024-10-10 19:20:55 +0200

    code

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,8 @@ export default defineConfig({
                     return `export default ${JSON.stringify(code)};`
                 }
             },
-        },],
+        },
+    ],
     define: {
         global: {},
     },
