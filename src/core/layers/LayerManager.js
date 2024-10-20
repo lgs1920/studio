fead236a5ccf1344a2a604ff84b0373ff538b3b0
chdenat <christian.denat@orange.fr>
2024-10-20 12:02:54 +0200

    - change data organisation + getters/setters

--- src/core/layers/LayerManager.js
+++ src/core/layers/LayerManager.js
@@ -5,7 +5,25 @@ export class LayerManager {
     static IGN_AERIAL = 'ign-photo'
     static OSM_PLAN = 'osm-plan'
 
-    #current = null
+    /**
+     * current layer
+     * @type {null|string}
+     */
+    #layer = null
+    /**
+     * current provider
+     * @type {null|string}
+     */
+    #provider = null
+    /**
+     * All providers
+     * @type {Map<any, any>}
+     */
+    #providers = new Map()
+    /**
+     * All layers
+     */
+    #layers = new Map()
 
     constructor() {
 
@@ -14,19 +32,41 @@ export class LayerManager {
             return LayerManager.instance
         }
 
-        this.#current = lgs.settings.layers.current
-        console.log('layer', lgs.settings.layers)
+        this.#layer = lgs.configuration.layers.current
+        this.#provider = this.#layer.split('-')[0]
+
+        // Let's transform layers and provider in order to be more efficient in code
+        lgs.configuration.layers.providers.forEach(provider => {
+            provider.layers.forEach(layer => {
+                layer.provider = provider.id
+                this.#layers.set(layer.id, layer)
+            })
+            this.#providers.set(provider.id, provider)
+
+        })
 
         LayerManager.instance = this
 
     }
 
-    get current() {
-        return this.#current
+    get layer() {
+        return this.#layer
     }
 
-    set current(current) {
-        this.#current = current
+    set layer(layer) {
+        this.#layer = layer
+    }
+
+    get layers() {
+        return this.#layers
+    }
+
+    get provider() {
+        return this.#provider
+    }
+
+    get providers() {
+        return this.#providers
     }
 
 }
