e89a92f66aa33916d5de578ca34c7ef8ee0bca8b
chdenat <christian.denat@orange.fr>
2024-12-15 19:33:50 +0100

    - adapt camera information to 2D

--- src/components/cesium/CameraAndTargetPanel/CameraAndTargetPanel.jsx
+++ src/components/cesium/CameraAndTargetPanel/CameraAndTargetPanel.jsx
@@ -28,54 +28,65 @@ export const CameraAndTargetPanel = () => {
                             <TextValueUI value={camera.target.latitude?.toFixed(5)}
                                          className={'camera-latitude'}
                                          text={'Lat:'}/>
-                            {/* hide elevation as it is not precise enough. TODO fix this  */}
-                            {/* <sl-icon library="fa" name={FA2SL.set(faMountains)}></sl-icon> */}
-                            {/* <TextValueUI value={camera.target.height?.toFixed()} */}
-                            {/*              className={'camera-altitude'} */}
-                            {/*              units={[meter, mile]}/> */}
-                        </div>
-                    </div>
-                </SlAnimation>
-            }
-
-            {ui.showCameraPosition &&
-                <SlAnimation size="small" easing="bounceInLeft" duration={1000} iterations={1}
-                             play={ui.showCameraPosition}>
-                    <div className="camera-data-panel lgs-card on-map">
-                        <sl-icon library="fa" name={FA2SL.set(faVideo)}></sl-icon>
-                        <div>
-                            <TextValueUI value={camera.position.longitude?.toFixed(5)}
-                                         className={'camera-longitude'}
-                                         text={'Lon:'}/>
-                            <TextValueUI value={camera.position.latitude?.toFixed(5)}
-                                         className={'camera-latitude'}
-                                         text={'Lat:'}/>
                             <sl-icon library="fa" name={FA2SL.set(faMountains)}></sl-icon>
-                            <TextValueUI value={camera.position?.height?.toFixed()}
+                            <TextValueUI value={camera.target.height?.toFixed()}
                                          className={'camera-altitude'}
                                          units={[meter, mile]}/>
+                            {__.ui.sceneManager.is2D &&
+                                <>
+                                    <sl-icon library="fa" name={FA2SL.set(faVideo)}></sl-icon>
+                                    <TextValueUI value={camera.position?.height?.toFixed()}
+                                                 className={'camera-altitude'}
+                                                 units={[meter, mile]}/>
+                                </>
+                            }
                         </div>
                     </div>
                 </SlAnimation>
             }
-            {ui.showCameraHPR &&
-                <SlAnimation size="small" easing="bounceInLeft" duration={1000} iterations={1} play={ui.showCameraHPR}>
-                    <>
-                        <div className="camera-data-panel lgs-card on-map">
-                            <sl-icon library="fa" name={FA2SL.set(faAngle)}></sl-icon>
-                            <div>
-                                <TextValueUI value={camera.position.heading?.toFixed()}
-                                             className={'camera-heading'} text={'Heading:'} units={'°'}/>
-
-                                <TextValueUI value={(camera.position?.pitch)?.toFixed()}
-                                             className={'camera-pitch'} text={'Pitch:'} units={'°'}/>
-
-                                <TextValueUI value={(camera.position?.roll)?.toFixed()}
-                                             className={'camera-roll'} text={'Roll:'} units={'°'}/>
+            {!__.ui.sceneManager.is2D &&
+                <>
+                    {ui.showCameraPosition &&
+                        <SlAnimation size="small" easing="bounceInLeft" duration={1000} iterations={1}
+                                     play={ui.showCameraPosition}>
+                            <div className="camera-data-panel lgs-card on-map">
+                                <sl-icon library="fa" name={FA2SL.set(faVideo)}></sl-icon>
+                                <div>
+                                    <TextValueUI value={camera.position.longitude?.toFixed(5)}
+                                                 className={'camera-longitude'}
+                                                 text={'Lon:'}/>
+                                    <TextValueUI value={camera.position.latitude?.toFixed(5)}
+                                                 className={'camera-latitude'}
+                                                 text={'Lat:'}/>
+                                    <sl-icon library="fa" name={FA2SL.set(faMountains)}></sl-icon>
+                                    <TextValueUI value={camera.position?.height?.toFixed()}
+                                                 className={'camera-altitude'}
+                                                 units={[meter, mile]}/>
+                                </div>
                             </div>
-                        </div>
-                    </>
-                </SlAnimation>
+                        </SlAnimation>
+                    }
+                    {ui.showCameraHPR &&
+                        <SlAnimation size="small" easing="bounceInLeft" duration={1000} iterations={1}
+                                     play={ui.showCameraHPR}>
+                            <>
+                                <div className="camera-data-panel lgs-card on-map">
+                                    <sl-icon library="fa" name={FA2SL.set(faAngle)}></sl-icon>
+                                    <div>
+                                        <TextValueUI value={camera.position.heading?.toFixed()}
+                                                     className={'camera-heading'} text={'Heading:'} units={'°'}/>
+
+                                        <TextValueUI value={(camera.position?.pitch)?.toFixed()}
+                                                     className={'camera-pitch'} text={'Pitch:'} units={'°'}/>
+
+                                        <TextValueUI value={(camera.position?.roll)?.toFixed()}
+                                                     className={'camera-roll'} text={'Roll:'} units={'°'}/>
+                                    </div>
+                                </div>
+                            </>
+                        </SlAnimation>
+                    }
+                </>
             }
         </div>
     )
