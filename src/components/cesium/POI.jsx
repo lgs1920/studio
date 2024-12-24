098366da8c5f26cea8513f9a1775db8fe55e66b5
chdenat <christian.denat@orange.fr>
2024-12-24 15:42:14 +0100

    - Add degrees,minutes,seconds

--- src/components/cesium/POI.jsx
+++ src/components/cesium/POI.jsx
@@ -1,6 +1,7 @@
 import { ELEVATION_UNITS }     from '@Utils/UnitUtils'
 import { Cartesian3 }          from 'cesium'
 import { useEffect, useState } from 'react'
+import { UIUtils }             from '../../Utils/UIUtils'
 import { TextValueUI }         from '../TextValueUI/TextValueUI'
 
 export const POI = ({point}) => {
@@ -34,9 +35,15 @@ export const POI = ({point}) => {
         <div className={'poi-on-map slide-in-from-top-bounced'}
              style={{bottom: `${window.innerHeight - pixels.y}px`, left: `${pixels.x}px`}}>
             <h3>{point.title}</h3>
-            <div className="poi-coordinates">
-                <TextValueUI text={'Elevation: '} value={point.elevation} format={'%d'} units={ELEVATION_UNITS}/>
-                <span>[{point.longitude}, {point.latitude}]</span>
+            <div className="poi-full-coordinates">
+                {point.elevation &&
+                    <TextValueUI text={'Elevation: '} value={point.elevation} format={'%d'} units={ELEVATION_UNITS}/>
+                }
+                <div className="poi-coordinates">
+                    <span>{UIUtils.toDMS(point.latitude)}, {UIUtils.toDMS(point.longitude)}</span><br/>
+                    <span>[{point.latitude}, {point.longitude}]</span><br/>
+                </div>
+
             </div>
         </div>
     )
