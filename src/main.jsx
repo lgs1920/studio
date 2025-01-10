88b64ec3d66980973676c997270af12cd7b892cc
chdenat <christian.denat@orange.fr>
2025-01-10 19:36:14 +0100

    - fix hidden content and avoid zoom

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,8 @@
 import './assets/css/theme.css'
 import './assets/css/light.css'
-//import { StrictMode }  from 'react'
 import { createRoot } from 'react-dom/client'
 import { LGS1920 }    from './LGS1920.jsx'
-import './assets/css/app.css?v=1.0.3'
+import './assets/css/app.css?v=1.0.5'
 import './assets/css/animations.css'
 
 /**
@@ -11,7 +10,5 @@ import './assets/css/animations.css'
  */
 
 createRoot(document.getElementById('lgs1920-container')).render(
-    // <StrictMode>
     <LGS1920/>,
-    // </StrictMode>,
 )
