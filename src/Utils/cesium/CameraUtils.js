214b68dc9b5de4ea0b4ef2cb81ffe8e320315e2b
chdenat <christian.denat@orange.fr>
2025-01-23 18:55:53 +0100

    - code

--- src/Utils/cesium/CameraUtils.js
+++ src/Utils/cesium/CameraUtils.js
@@ -3,9 +3,9 @@ import { Cartesian2, Cartesian3, Cartographic, Ellipsoid, Math as M, Matrix4, Sc
 
 export class CameraUtils {
 
-    static lookAt = (camera, center, hpr) => {
+    static lookAt = (camera, target, hpr) => {
         // Lock camera to a point
-        const point = new Cartesian3(center.x, center.y, center.z)
+        const point = Cesium.Cartesian3.fromDegrees(target.longitude, target.latitude, target.height)
         camera.lookAtTransform(Transforms.eastNorthUpToFixedFrame(point), hpr)
     }
 
