195bc2151120ca5f7fe249ab3ec77f3b8430aef2
chdenat <christian.denat@orange.fr>
2025-02-06 11:07:09 +0100

    - new  label

--- src/components/MainUI/FocusButton.jsx
+++ src/components/MainUI/FocusButton.jsx
@@ -21,7 +21,7 @@ export const FocusButton = (props) => {
     }
     return (
         <>
-            <SlTooltip hoist placement={placement} content="Focus on the current journey">
+            <SlTooltip hoist placement={placement} content={'Reset the Focus'}>
                 <SlButton size={'small'} className={'square-icon'} id={'focus-on-current-journey'}
                           onClick={focusOnJourney}>
                     <SlIcon slot="prefix" library="fa" name={FA2SL.set(faCrosshairsSimple)}/>
