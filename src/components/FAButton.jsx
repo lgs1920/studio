b5b405eafd4c90c465041855a4afdfeaf55e417e
chdenat <christian.denat@orange.fr>
2025-03-28 11:17:57 +0100

    - can add classnames
    Signed-off-by: chdenat <christian.denat@orange.fr>

--- src/components/FAButton.jsx
+++ src/components/FAButton.jsx
@@ -1,9 +1,10 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import classNames from 'classnames'
 
 export const FAButton = (props) => {
 
     return (
-        <div className="fa-icon-button" {...props.ref} >
+        <div className={classNames('fa-icon-button', props.className)} {...props.ref} >
             <FontAwesomeIcon {...props} />
         </div>
     )
