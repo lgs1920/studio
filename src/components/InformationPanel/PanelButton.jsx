7d46cac2079056ffe8fa83aafc7ea8cfca573b3e
chdenat <christian.denat@orange.fr>
2025-03-28 10:41:19 +0100

    - fix tooltip placement
    Signed-off-by: chdenat <christian.denat@orange.fr>

--- src/components/InformationPanel/PanelButton.jsx
+++ src/components/InformationPanel/PanelButton.jsx
@@ -4,12 +4,14 @@ import { SlButton, SlIcon, SlTooltip } from '@shoelace-style/shoelace/dist/react
 import { FA2SL }                       from '@Utils/FA2SL'
 import React                           from 'react'
 import './style.css'
+import { useSnapshot } from 'valtio'
 
 export const PanelButton = () => {
     const infoPanelStore = lgs.mainProxy.components.informationPanel
+    const settings = useSnapshot(lgs.settings.ui.menu)
 
     return (<>
-        <SlTooltip hoist placement="right" content="Show Information">
+        <SlTooltip hoist placement={settings.toolBar.fromStart ? 'right' : 'left'} content="Show Information">
             <SlButton className={'square-button'} size="small" id={'open-info-panel'}
                       onClick={() => __.ui.drawerManager.toggle(INFO_DRAWER)}>
                 <SlIcon slot="prefix" library="fa" name={FA2SL.set(faCircleInfo)}></SlIcon>
--- src/components/MainUI/SupportUIButton.jsx
+++ src/components/MainUI/SupportUIButton.jsx
@@ -1,13 +1,16 @@
 import { faMessageQuestion }           from '@fortawesome/pro-regular-svg-icons'
 import { SlButton, SlIcon, SlTooltip } from '@shoelace-style/shoelace/dist/react'
 import { FA2SL } from '@Utils/FA2SL.js'
+import { useSnapshot } from 'valtio'
 
 
 export const SupportUIButton = () => {
     const supportUIStore = lgs.mainProxy.components.mainUI.support
+    const settings = useSnapshot(lgs.settings.ui.menu)
+
     return (
         <>
-            <SlTooltip hoist placement="right" content="Open Help">
+            <SlTooltip hoist placement={settings.toolBar.fromStart ? 'right' : 'left'} content="Open Help">
                 <SlButton size={'small'} className={'square-button'} id={'launch-the-support'}
                           onClick={() => supportUIStore.visible = !supportUIStore.visible}>
                     <SlIcon slot="prefix" library="fa" name={FA2SL.set(faMessageQuestion)}/>
