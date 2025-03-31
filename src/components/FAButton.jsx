971f5fd6da9919cdd47646c98d194d2f2d6f6fb7
chdenat <christian.denat@orange.fr>
2025-03-31 18:24:36 +0200

    - onclick
    Signed-off-by: chdenat <christian.denat@orange.fr>

--- src/components/FAButton.jsx
+++ src/components/FAButton.jsx
@@ -2,9 +2,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import classNames from 'classnames'
 
 export const FAButton = (props) => {
-    const {className, id, ref, ...rest} = props
+    const {className, onClick, id, ref, ...rest} = props
     return (
-        <div className={classNames('fa-icon-button', className)} ref={ref} id={id}>
+        <div className={classNames('fa-icon-button', className)} ref={ref} id={id} onClick={onClick}>
             <FontAwesomeIcon {...rest} />
         </div>
     )
