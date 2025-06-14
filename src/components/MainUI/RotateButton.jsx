29eac0413d2fed360538cc902097da0e066b98a7
chdenat <christian.denat@orange.fr>
2025-06-14 17:22:01 +0200

    render optimisation
    Signed-off-by: chdenat <christian.denat@orange.fr>

--- src/components/MainUI/RotateButton.jsx
+++ src/components/MainUI/RotateButton.jsx
@@ -7,68 +7,87 @@
  * Author : LGS1920 Team
  * email: contact@lgs1920.fr
  *
- * Created on: 2025-05-19
- * Last modified: 2025-05-19
+ * Created on: 2025-06-14
+ * Last modified: 2025-06-14
  *
  *
  * Copyright © 2025 LGS1920
  ******************************************************************************/
 
 import { NONE, POI_STANDARD_TYPE } from '@Core/constants'
-import { MapPOI }                  from '@Core/MapPOI'
-import { POI }    from '@Core/POI'
-import { faArrowRotateRight }          from '@fortawesome/pro-regular-svg-icons'
 import { SlButton, SlIcon, SlTooltip } from '@shoelace-style/shoelace/dist/react'
-import { FA2SL }                       from '@Utils/FA2SL.js'
-import React                           from 'react'
-import { useSnapshot }                 from 'valtio'
+import { FA2SL }                      from '@Utils/FA2SL.js'
+import { faArrowRotateRight } from '@fortawesome/pro-regular-svg-icons'
+import { memo, useCallback, useMemo } from 'react'
+import { useSnapshot }                from 'valtio'
 
+// Static icon name to avoid recalculation
+const ICON_NAME = FA2SL.set(faArrowRotateRight)
+const FOCUS_TARGET = 'target'
 
-export const RotateButton = (props) => {
-    const $mainUI = lgs.stores.main.components.mainUI
-    const rotate = useSnapshot($mainUI).rotate
-    const camera = useSnapshot(lgs.mainProxy.components.camera)
-    const $pois = lgs.mainProxy.components.pois
-    const pois = useSnapshot($pois)
+/**
+ * A memoized React component for toggling map rotation around a target.
+ * @param {Object} props - Component props
+ * @param {string} [props.tooltip='top'] - Tooltip placement (e.g., 'top', 'bottom')
+ * @returns {JSX.Element} The rendered component
+ */
+export const RotateButton = memo(({tooltip = 'top'}) => {
+    // Targeted snapshots to minimize re-renders
+    const {rotate} = useSnapshot(lgs.stores.main.components.mainUI)
+    const {target, position} = useSnapshot(lgs.stores.main.components.camera)
+    const {list, current} = useSnapshot(lgs.stores.main.components.pois)
 
-    const DUMMY_TARGET = 'target'
+    // Memoized scene target check
+    const isPOITarget = useMemo(() => {
+        return __.ui.sceneManager.target?.element === POI_STANDARD_TYPE
+    }, [__.ui.sceneManager.target])
 
-    const handleRotation = async () => {
-        if (rotate.running) {
-            await __.ui.cameraManager.stopRotate()
-            if (__.ui.sceneManager.target?.element === POI_STANDARD_TYPE) {
-                // Object.assign(__.ui.sceneManager.target,{animated:false})
-                Object.assign($pois.list.get(pois.current), {animated: false})
+    /**
+     * Toggles map rotation and updates POI animation state if applicable.
+     * @returns {Promise<void>}
+     */
+    const handleRotation = useCallback(async () => {
+        const poi = isPOITarget && current ? list.get(current) : null
+
+        try {
+            if (rotate.running) {
+                await __.ui.cameraManager.stopRotate()
+                if (poi && poi.animated !== false) {
+                    poi.animated = false
+                }
+            }
+            else {
+                await __.ui.sceneManager.focus(target, {
+                    heading:    position.heading,
+                    pitch:      position.pitch,
+                    roll:       position.roll,
+                    range:      position.range,
+                    infinite:   true,
+                    rotate:     true,
+                    flyingTime: 0,
+                    target:     FOCUS_TARGET,
+                })
+                if (poi && poi.animated !== true) {
+                    poi.animated = true
+                }
             }
         }
-        else {
-            __.ui.sceneManager.focus(camera.target, {
-                heading:    camera.position.heading,
-                pitch:      camera.position.pitch,
-                roll:       camera.position.roll,
-                range: camera.position.range,
-                infinite:   true,
-                rotate:     true,
-                flyingTime: 0,    // no move, no time ! We're on target
-                target: DUMMY_TARGET
-                ,
-            })
-            if (__.ui.sceneManager.target?.element === POI_STANDARD_TYPE) {
-                Object.assign($pois.list.get(pois.current), {animated: true})
-            }
+        catch (error) {
+            console.error('Failed to toggle map rotation:', error)
         }
-    }
+    }, [rotate.running, target, position, current, isPOITarget, list])
 
     return (
-        <>
-            <SlTooltip hoist placement={props.tooltip}
-                       content={rotate.running ? 'Stop Map Rotation' : 'Start Map Rotation'}>
-                <SlButton size={'small'} className={'square-button'} id={'launch-rotation'}
-                          onClick={handleRotation} loading={rotate.running}>
-                    <SlIcon slot="prefix" library="fa" name={FA2SL.set(faArrowRotateRight)}/>
-
-                </SlButton>
-            </SlTooltip>
-        </>
+        <SlTooltip hoist placement={tooltip} content={rotate.running ? 'Stop Map Rotation' : 'Start Map Rotation'}>
+            <SlButton
+                size="small"
+                className="square-button"
+                id="launch-rotation"
+                onClick={handleRotation}
+                loading={rotate.running}
+            >
+                <SlIcon slot="prefix" library="fa" name={ICON_NAME}/>
+            </SlButton>
+        </SlTooltip>
     )
-}
+})
