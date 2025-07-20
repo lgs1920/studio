3d0591cfaf3117d4c4bc5489d406411a7dfa4dfe
chdenat <christian.denat@orange.fr>
2025-07-20 12:18:41 +0200

    remove useless
    Signed-off-by: chdenat <christian.denat@orange.fr>

--- src/core/ui/cropper/CropperManager.js
+++ src/core/ui/cropper/CropperManager.js
@@ -7,8 +7,8 @@
  * Author : LGS1920 Team
  * email: contact@lgs1920.fr
  *
- * Created on: 2025-07-19
- * Last modified: 2025-07-19
+ * Created on: 2025-07-20
+ * Last modified: 2025-07-20
  *
  *
  * Copyright © 2025 LGS1920
@@ -27,8 +27,7 @@ export class CropperManager {
             '9x16':    faRectangleVertical,
             '16x9':    faRectangle,
             '1x1':     faSquare,
-            'free':    faCropSimple,
-            'unknown': faCropSimple,
+            '4x3': faCropSimple,
         }
 
 
