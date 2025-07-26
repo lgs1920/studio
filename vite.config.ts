3ef6ccbd2c5ddf474cef9c86cdd12d229c7f1cf0
chdenat <christian.denat@orange.fr>
2025-07-26 17:29:43 +0200

    exclude FFMpeg from checks
    Signed-off-by: chdenat <christian.denat@orange.fr>

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@
  * Author : LGS1920 Team
  * email: contact@lgs1920.fr
  *
- * Created on: 2025-06-30
- * Last modified: 2025-06-30
+ * Created on: 2025-07-26
+ * Last modified: 2025-07-26
  *
  *
  * Copyright © 2025 LGS1920
@@ -96,5 +96,12 @@ export default defineConfig({
             },
 
         ]
-    }
+    },
+    optimizeDeps: {
+        exclude: [
+            '@ffmpeg/core', // Exclude FFmpeg core to avoid worker optimization issues
+            '@ffmpeg/ffmpeg', // Exclude FFmpeg main module
+            '@ffmpeg/util', // Exclude FFmpeg utilities
+        ],
+    },
 })
