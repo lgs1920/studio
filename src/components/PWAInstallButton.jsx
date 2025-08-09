8a2770d25f93ce6f16f0c41afd44f75c32dcbd2d
chdenat <christian.denat@orange.fr>
2025-08-09 19:07:16 +0200

    Branch 0.9.0 deployed on staging-0.9.0-0.9.0-20250809T170641!

--- src/components/PWAInstallButton.jsx
+++ src/components/PWAInstallButton.jsx
@@ -1,5 +1,21 @@
-import { useState, useEffect } from 'react';
-import { SlButton } from '@shoelace-style/shoelace/dist/react';
+/*******************************************************************************
+ *
+ * This file is part of the LGS1920/studio project.
+ *
+ * File: PWAInstallButton.js
+ *
+ * Author : LGS1920 Team
+ * email: contact@lgs1920.fr
+ *
+ * Created on: 2025-08-09
+ * Last modified: 2025-08-09
+ *
+ *
+ * Copyright © 2025 LGS1920
+ ******************************************************************************/
+
+import { SlButton }            from '@shoelace-style/shoelace/dist/react'
+import { useEffect, useState } from 'react'
 
 export const PWAInstallButton = () => {
   const [deferredPrompt, setDeferredPrompt] = useState(null);
@@ -35,7 +51,7 @@ export const PWAInstallButton = () => {
 
   return (
     <SlButton size="small" onClick={handleInstall}>
-      Installer l'app
+        {'Install App'}
     </SlButton>
   );
 };
