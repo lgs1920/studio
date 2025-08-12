1289866be45a94f57f4e5459e43c67b5eafc545f
chdenat <christian.denat@orange.fr>
2025-08-12 10:55:54 +0200

    Branch 0.9.0 deployed on staging-0.9.0-0.9.0-20250812T085536!

---
+++ src/core/ui/AppUpdateManager.js
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ *
+ * This file is part of the LGS1920/studio project.
+ *
+ * File: AppUpdateManager.js
+ *
+ * Author : LGS1920 Team
+ * email: contact@lgs1920.fr
+ *
+ * Created on: 2025-08-11
+ * Last modified: 2025-08-11
+ *
+ *
+ * Copyright © 2025 LGS1920
+ ******************************************************************************/
+
