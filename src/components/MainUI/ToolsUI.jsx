729706253f4cee6643d182965cf61b62ede003a0
chdenat <christian.denat@orange.fr>
2025-09-22 23:11:34 +0200

    fix components
    Signed-off-by: chdenat <christian.denat@orange.fr>

--- src/components/MainUI/ToolsUI.jsx
+++ src/components/MainUI/ToolsUI.jsx
@@ -7,8 +7,8 @@
  * Author : LGS1920 Team
  * email: contact@lgs1920.fr
  *
- * Created on: 2025-09-14
- * Last modified: 2025-09-14
+ * Created on: 2025-09-22
+ * Last modified: 2025-09-22
  *
  *
  * Copyright © 2025 LGS1920
@@ -16,6 +16,7 @@
 import { CameraAndTargetPanel } from '@Components/cesium/CameraAndTargetPanel/CameraAndTargetPanel'
 import { ContextMenuHook }      from '@Components/MainUI/ContextMenuHook'
 import { VideoFPSSelector }     from '@Components/MainUI/video/VideoFPSSelector'
+import { VideoFPSToolbar } from '@Components/MainUI/video/VideoFPSToolbar'
 import { VideoQualitySelector } from '@Components/MainUI/video/VideoQualitySelector'
 import { VideoRecorderToolbar } from '@Components/MainUI/video/VideoRecorderToolbar'
 import { VideoSettingsInfo } from '@Components/MainUI/video/VideoSettingsInfo'
