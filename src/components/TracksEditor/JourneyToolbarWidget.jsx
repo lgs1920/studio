8fa82d2318df0a439baa07ef4c1968f084debc0c
chdenat <christian.denat@orange.fr>
2025-09-25 11:48:22 +0200

    use widget
    Signed-off-by: chdenat <christian.denat@orange.fr>

---
+++ src/components/TracksEditor/JourneyToolbarWidget.jsx
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ *
+ * This file is part of the LGS1920/studio project.
+ *
+ * File: JourneyToolbarWidget.jsx
+ *
+ * Author : LGS1920 Team
+ * email: contact@lgs1920.fr
+ *
+ * Created on: 2025-09-25
+ * Last modified: 2025-09-25
+ *
+ *
+ * Copyright © 2025 LGS1920
+ ******************************************************************************/
+
+
+import { DraggableUIWidget } from '@Components/MainUI/DraggableUIWidget'
+import { JourneyToolbar }    from '@Editor/JourneyToolbar'
+import React, { useMemo }    from 'react'
+import { useSnapshot }       from 'valtio'
+
+/**
+ * Component for selecting video quality with draggable toolbar
+ * @component
+ * @returns {JSX.Element} Draggable video quality selector UI
+ */
+export const JourneyToolbarWidget = () => {
+
+    const $journeyEditor = lgs.mainProxy.components.journeyEditor
+    const journeyEditor = useSnapshot($journeyEditor)
+
+    const $journeyToolbar = lgs.settings.ui.journeyToolbar
+    const journeyToolbar = useSnapshot($journeyToolbar)
+
+    // Stabilize config with useMemo
+    const config = useMemo(() => {
+        const myConfig = {
+            top:      '70%',
+            opacity:  lgs.settings.ui.toolbars.opacity,
+            left:     '50%',
+            attachTo: 'bottom',
+        }
+        return myConfig
+    }, [])
+
+    return (
+        <DraggableUIWidget isVisible={journeyEditor.list.length > 0 && journeyToolbar.show}
+                           config={config} className="">
+            <JourneyToolbar/>
+        </DraggableUIWidget>
+    )
+}
