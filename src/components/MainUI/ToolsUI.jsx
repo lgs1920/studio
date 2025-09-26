9df5d12fe3dd40dc2860f336295af6e3eb4c2123
chdenat <christian.denat@orange.fr>
2025-09-26 10:27:40 +0200

    Code
    Signed-off-by: chdenat <christian.denat@orange.fr>

--- src/components/MainUI/ToolsUI.jsx
+++ src/components/MainUI/ToolsUI.jsx
@@ -7,8 +7,8 @@
  * Author : LGS1920 Team
  * email: contact@lgs1920.fr
  *
- * Created on: 2025-09-25
- * Last modified: 2025-09-25
+ * Created on: 2025-09-26
+ * Last modified: 2025-09-26
  *
  *
  * Copyright © 2025 LGS1920
@@ -16,14 +16,12 @@
 import { CameraAndTargetPanel } from '@Components/cesium/CameraAndTargetPanel/CameraAndTargetPanel'
 import { ContextMenuHook }    from '@Components/MainUI/ContextMenuHook'
 import { VideoFPSWidget }     from '@Components/MainUI/video/VideoFPSWidget'
-import { VideoFPSToolbar }    from '@Components/MainUI/video/VideoFPSToolbar'
 import { VideoQualityWidget } from '@Components/MainUI/video/VideoQualityWidget'
 import { VideoRecorderToolbar } from '@Components/MainUI/video/VideoRecorderToolbar'
+import { VideoRecordingSettingsWidget } from '@Components/MainUI/video/VideoRecordingSettingsWidget'
 import { VideoSettingsInfo } from '@Components/MainUI/video/VideoSettingsInfo'
 import { Cropper }              from '@Components/ToolsUI/cropper/Cropper'
-import { CropRatioSelector }    from '@Components/ToolsUI/cropper/CropRatioSelector'
 import { MapPOIContextMenu }    from '@Components/MainUI/MapPOI/MapPOIContextMenu'
-import { VideoRecordingSettingsToolbar } from '@Components/MainUI/video/VideoRecordingSettingsToolbar'
 import { JourneyToolbarWidget } from '@Editor/JourneyToolbarWidget'
 import { useSnapshot }          from 'valtio/index'
 
@@ -38,9 +36,9 @@ export const ToolsUI = () => {
                 <>
                     <Cropper overlay source={lgs.canvas} store={$cropper}
                              options={{infoComponent: <VideoSettingsInfo/>}}/>
-                    <VideoRecordingSettingsToolbar store={$cropper}/>
-                    <VideoQualityWidget store={$cropper}/>
-                    <VideoFPSWidget store={$cropper}/>
+                    <VideoRecordingSettingsWidget/>
+                    <VideoQualityWidget/>
+                    <VideoFPSWidget/>
                 </>
             ) : (
                  <>
