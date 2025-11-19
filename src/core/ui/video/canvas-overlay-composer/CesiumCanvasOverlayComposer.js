5aad85e8240ab59d8fc50f612be7d553dfa70743
chdenat <christian.denat@orange.fr>
2025-11-19 10:00:55 +0100

    Branch 0.9.0-canvases-mix-for-video deployed on staging-0.9.0-0.9.0-canvases-mix-for-video-20251119T090027!

---
+++ src/core/ui/video/canvas-overlay-composer/CesiumCanvasOverlayComposer.js
@@ -0,0 +1,16 @@
+/*******************************************************************************
+ *
+ * This file is part of the LGS1920/studio project.
+ *
+ * File: CesiumCanvasOverlayComposer.js
+ *
+ * Author : LGS1920 Team
+ * email: contact@lgs1920.fr
+ *
+ * Created on: 2025-11-19
+ * Last modified: 2025-11-19
+ *
+ *
+ * Copyright © 2025 LGS1920
+ ******************************************************************************/
+
