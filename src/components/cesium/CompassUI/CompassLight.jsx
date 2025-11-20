18518a50f8993f79ab4ccafb5a653d29e61be9a2
chdenat <christian.denat@orange.fr>
2025-11-20 19:05:01 +0100

    Fixes needle rotation
    Signed-off-by: chdenat <christian.denat@orange.fr>

--- src/components/cesium/CompassUI/CompassLight.jsx
+++ src/components/cesium/CompassUI/CompassLight.jsx
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ *
+ * This file is part of the LGS1920/studio project.
+ *
+ * File: CompassLight.jsx
+ *
+ * Author : LGS1920 Team
+ * email: contact@lgs1920.fr
+ *
+ * Created on: 2025-11-20
+ * Last modified: 2025-11-20
+ *
+ *
+ * Copyright © 2025 LGS1920
+ ******************************************************************************/
+
 export const CompassLight = ({width = '100%', height = '100%', ref}) => {
 
 
@@ -7,9 +23,10 @@ export const CompassLight = ({width = '100%', height = '100%', ref}) => {
 
         <svg height={height} width={width} xmlns="http://www.w3.org/2000/svg"
              xmlnsXlink="http://www.w3.org/1999/xlink"
-             viewBox="0 0 512 512" xmlSpace="preserve">
+             viewBox="0 0 512 512" xmlSpace="preserve" style={{transform: 'rotate(-45deg)'}}>
             <g>
-                <g className="lgs-compass-needle" ref={ref} style={{rotate: '-45deg', scale: 1.2}}>
+                <g className="lgs-compass-needle" ref={ref}
+                   style={{transform: 'scale(1.2)', transformOrigin: 'center'}}>
                     <path id="north" d="M296.327,296.354l-80.703-80.703l174.962-101.759c9.854-5.731,13.225-2.36,7.494,7.494
 					L296.327,296.354z"/>
                     <path id="south" d="M296.327,296.354L121.36,398.108c-9.854,5.731-13.225,2.36-7.494-7.494l101.759-174.962
