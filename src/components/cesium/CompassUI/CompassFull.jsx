00a19eeb1acc70fdb217f12bc88cbc540d31e497
chdenat <christian.denat@orange.fr>
2025-11-25 11:22:37 +0100

    SVG (CSS rules) uses only classes
    Signed-off-by: chdenat <christian.denat@orange.fr>

--- src/components/cesium/CompassUI/CompassFull.jsx
+++ src/components/cesium/CompassUI/CompassFull.jsx
@@ -7,8 +7,8 @@
  * Author : LGS1920 Team
  * email: contact@lgs1920.fr
  *
- * Created on: 2025-11-20
- * Last modified: 2025-11-20
+ * Created on: 2025-11-25
+ * Last modified: 2025-11-25
  *
  *
  * Copyright © 2025 LGS1920
@@ -57,9 +57,9 @@ export const CompassFull = ({width = '100%', height = '100%', ref}) => {
                     <g style={{transform: 'rotate(-45deg)', transformOrigin: 'center'}}>
                         <g className="lgs-compass-needle" ref={ref}
                            style={{transform: 'scale(1.2)', transformOrigin: 'center'}}>
-                            <path id="north" d="M296.327,296.354l-80.703-80.703l174.962-101.759c9.854-5.731,13.225-2.36,7.494,7.494
+                            <path className="lgs-compass-needle-north" d="M296.327,296.354l-80.703-80.703l174.962-101.759c9.854-5.731,13.225-2.36,7.494,7.494
 					L296.327,296.354z"/>
-                            <path id="south" d="M296.327,296.354L121.36,398.108c-9.854,5.731-13.225,2.36-7.494-7.494l101.759-174.962
+                            <path className="lgs-compass-needle-south" d="M296.327,296.354L121.36,398.108c-9.854,5.731-13.225,2.36-7.494-7.494l101.759-174.962
 					L296.327,296.354z"/>
                         </g>
                     </g>
