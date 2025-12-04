125617e15ef0ca228ee1a126f61e3cc73200339e
chdenat <christian.denat@orange.fr>
2025-12-04 16:06:36 +0100

    Forces a scene render after hide/show
    Signed-off-by: chdenat <christian.denat@orange.fr>

--- src/Utils/cesium/POIUtils.js
+++ src/Utils/cesium/POIUtils.js
@@ -7,8 +7,8 @@
  * Author : LGS1920 Team
  * email: contact@lgs1920.fr
  *
- * Created on: 2025-06-20
- * Last modified: 2025-06-20
+ * Created on: 2025-12-04
+ * Last modified: 2025-12-04
  *
  *
  * Copyright © 2025 LGS1920
@@ -132,7 +132,7 @@ export class POIUtils {
 
         // Update visibility state
         entity.show = poi.visible
-        lgs.viewer.scene.requestRender()
+        lgs.viewer.scene.render()
         return true
     }
 
