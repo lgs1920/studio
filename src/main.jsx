7a01298455d93ac8dd26ea78befa01abe5571d6b
chdenat <christian.denat@orange.fr>
2025-12-21 13:08:35 +0100

    Adds patch for SHoelace resize observer (to avoid  remove too early the observed element)
    Signed-off-by: chdenat <christian.denat@orange.fr>

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@
  * Author : LGS1920 Team
  * email: contact@lgs1920.fr
  *
- * Created on: 2025-06-27
- * Last modified: 2025-06-26
+ * Created on: 2025-12-21
+ * Last modified: 2025-12-21
  *
  *
  * Copyright © 2025 LGS1920
@@ -20,6 +20,18 @@ import { createRoot } from 'react-dom/client'
 import { LGS1920 }    from './LGS1920.jsx'
 import './assets/css/app.css?v=1.0.5'
 import './assets/css/animations.css'
+
+/**
+ * Patch pour Shoelace ResizeObserver bug
+ * https://github.com/shoelace-style/shoelace/issues/1690
+ */
+const originalUnobserve = ResizeObserver.prototype.unobserve
+ResizeObserver.prototype.unobserve = function (target) {
+    if (target && target instanceof Element) {
+        originalUnobserve.call(this, target)
+    }
+}
+
 /**
  * Let's go
  */
