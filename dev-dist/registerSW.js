4c536921e8a08a18d220499ac9dc0b79ccdb2b57
chdenat <christian.denat@orange.fr>
2026-01-12 17:41:41 +0100

    SW  management
    Signed-off-by: chdenat <christian.denat@orange.fr>

--- dev-dist/registerSW.js
+++ dev-dist/registerSW.js
@@ -7,8 +7,8 @@
  * Author : LGS1920 Team
  * email: contact@lgs1920.fr
  *
- * Created on: 2026-01-11
- * Last modified: 2026-01-11
+ * Created on: 2026-01-12
+ * Last modified: 2026-01-12
  *
  *
  * Copyright © 2026 LGS1920
