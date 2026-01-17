e7b2261f3ffa260e6b7df330c4d51759062e1c76
chdenat <christian.denat@orange.fr>
2026-01-17 17:28:22 +0100

    SW registering for development
    Signed-off-by: chdenat <christian.denat@orange.fr>

--- dev-dist/registerSW.js
+++ dev-dist/registerSW.js
@@ -7,8 +7,8 @@
  * Author : LGS1920 Team
  * email: contact@lgs1920.fr
  *
- * Created on: 2026-01-14
- * Last modified: 2026-01-14
+ * Created on: 2026-01-17
+ * Last modified: 2026-01-17
  *
  *
  * Copyright © 2026 LGS1920
