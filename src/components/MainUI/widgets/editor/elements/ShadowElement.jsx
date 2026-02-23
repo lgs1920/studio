c3f986633d0c91d445c3d3dc950d42bcecc8433a
chdenat <christian.denat@orange.fr>
2026-02-23 12:13:24 +0100

    Fixes text shadow element
    Signed-off-by: chdenat <christian.denat@orange.fr>

--- src/components/MainUI/widgets/editor/elements/ShadowElement.jsx
+++ src/components/MainUI/widgets/editor/elements/ShadowElement.jsx
@@ -7,8 +7,8 @@
  * Author : LGS1920 Team
  * email: contact@lgs1920.fr
  *
- * Created on: 2026-02-06
- * Last modified: 2026-02-06
+ * Created on: 2026-02-23
+ * Last modified: 2026-02-23
  *
  *
  * Copyright © 2026 LGS1920
@@ -17,35 +17,37 @@
 import { SlColorPicker, SlOption, SlRange, SlSelect, SlSwitch } from '@shoelace-style/shoelace/dist/react'
 import React                                                    from 'react'
 
-export const ShadowElement = ({element, swatches, getColor, updateValue}) => (
-    <>
-        <SlSwitch align-right size="x-small" checked={element.text?.shadow?.show ?? false}
-                  onSlInput={(e) => updateValue('text.shadow.show', e.target.checked)}>
-            <label>{'Text elevation'}</label>
-        </SlSwitch>
+export const ShadowElement = ({element, swatches, getColor, updateValue}) => {
+    return (
+        <>
+            <SlSwitch align-right size="x-small" checked={element.text?.shadow?.show ?? false}
+                      onSlInput={(e) => updateValue('text.shadow.show', e.target.checked)}>
+                <label>{'Text elevation'}</label>
+            </SlSwitch>
 
-        {element.text?.shadow?.show && (
-            <div className="drawer-horizontal-line three-columns">
-                <div className="drawer-horizontal-element">
-                    <SlColorPicker size="small" swatches={swatches} value={element.text.shadow.color}
-                                   onSlInput={(e) => updateValue('text.shadow.color', e.target.value)}/>
+            {element.text?.shadow?.show && (
+                <div className="drawer-horizontal-line three-columns">
+                    <div className="drawer-horizontal-element">
+                        <SlColorPicker size="small" swatches={swatches} value={element.text.shadow.color}
+                                       onSlInput={(e) => updateValue('text.shadow.color', e.target.value)}/>
+                    </div>
+                    <div className="drawer-horizontal-element">
+                        <SlSelect hoist size="small" value={element.text.shadow?.value ?? 'normal'}
+                                  style={{marginLeft: 'auto', width: '6.5rem'}}
+                                  onSlChange={(e) => updateValue('text.shadow.value', e.target.value)}>
+                            <SlOption value="small">{'Small'}</SlOption>
+                            <SlOption value="normal">{'Medium'}</SlOption>
+                            <SlOption value="large">{'Large'}</SlOption>
+                        </SlSelect>
+                    </div>
+                    <div className="drawer-horizontal-element xlarge-element">
+                        <SlRange label="Opacity" align-right tooltip="top" min="0" max="1" step="0.05"
+                                 tooltipFormatter={value => `${Math.floor(value * 100)}%`}
+                                 value={element.text.shadow?.opacity ?? 1}
+                                 onSlInput={(e) => updateValue('text.shadow.opacity', parseFloat(e.target.value))}/>
+                    </div>
                 </div>
-                <div className="drawer-horizontal-element">
-                    <SlSelect hoist size="small" value={element.text.shadow?.value ?? 'normal'}
-                              style={{marginLeft: 'auto', width: '6.5rem'}}
-                              onSlChange={(e) => updateValue('text.shadow.value', e.target.value)}>
-                        <SlOption value="small">{'Small'}</SlOption>
-                        <SlOption value="normal">{'Medium'}</SlOption>
-                        <SlOption value="large">{'Large'}</SlOption>
-                    </SlSelect>
-                </div>
-                <div className="drawer-horizontal-element xlarge-element">
-                    <SlRange label="Opacity" align-right tooltip="top" min="0" max="1" step="0.05"
-                             tooltipFormatter={value => `${Math.floor(value * 100)}%`}
-                             value={element.text.shadow?.opacity ?? 1}
-                             onSlInput={(e) => updateValue('text.shadow.opacity', parseFloat(e.target.value))}/>
-                </div>
-            </div>
-        )}
-    </>
-)
+            )}
+        </>
+    )
+}
