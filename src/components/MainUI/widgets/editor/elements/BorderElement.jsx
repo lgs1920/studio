2eaf5b716700252879bceccc9af4ed4fefd3f563
chdenat <christian.denat@orange.fr>
2026-06-18 11:28:26 +0200

    Clean up widget editor border styles
    Signed-off-by: chdenat <christian.denat@orange.fr>

--- src/components/MainUI/widgets/editor/elements/BorderElement.jsx
+++ src/components/MainUI/widgets/editor/elements/BorderElement.jsx
@@ -7,16 +7,16 @@
  * Author : LGS1920 Team
  * email: contact@lgs1920.fr
  *
- * Created on: 2026-05-10
- * Last modified: 2026-05-10
+ * Created on: 2026-06-18
+ * Last modified: 2026-06-18
  *
  *
  * Copyright © 2026 LGS1920
  ******************************************************************************/
 
-import { WIDGET_RADIUS }               from '@Core/constants'
-import { WaColorPicker, WaOption, WaSelect, WaSlider, WaSwitch } from '@web.awesome.me/webawesome-pro/dist/react'
-import { ScaleSwitchElement }          from './ScaleSwitchElement'
+import { WaColorPicker, WaSlider, WaSwitch } from '@web.awesome.me/webawesome-pro/dist/react'
+import { RadiusElement }                     from './RadiusElement'
+import { ScaleSwitchElement }                from './ScaleSwitchElement'
 import { formatSliderPercent, sanitizeNumericControlValue } from './sliderUtils'
 
 /**
@@ -35,24 +35,10 @@ export const BorderElement = ({
                                   showRadiusScale = true,
                                   sanitizeSliderValue = sanitizeNumericControlValue,
                               }) => {
-    const currentRadius = element.border?.radius ?? 'none'
     const borderOpacity = sanitizeSliderValue(element.border?.opacity, 1, {min: 0, max: 1})
 
-    /**
-     * Handles radius selection and updates the pill flag.
-     */
-    const handleRadiusChange = (e) => {
-        const val = Array.isArray(e.target.value) ? e.target.value[0] : e.target.value
-
-        // Update the radius name (e.g., 'm', 'l', 'pill')
-        updateValue('border.radius', val)
-
-        // Update the pill boolean based on the selection
-        updateValue('border.pill', val === 'pill')
-    }
-
     return (
-        <div>
+        <div className="lgs-border-element">
             <WaSwitch
                 label-at-start
                 size="xs"
@@ -63,99 +49,65 @@ export const BorderElement = ({
             </WaSwitch>
 
             {element.border?.show && (
-                <>
-                    <div className="lgs-widget-color-control-grid lgs-widget-border-control-grid">
-                        <div className="lgs-widget-color-control-color">
-                            <WaColorPicker
-                                size="s"
-                                swatches={swatches}
-                                value={getColor(element.border)}
-                                onInput={(e) => updateValue('border.color', e.target.value)}
-                            />
-                        </div>
-                        <div className="lgs-widget-border-control-row">
-                            <div className="drawer-horizontal-element lgs-widget-border-control">
-                                <WaSlider
-                                    withTooltip
-                                    size="s"
-                                    label="Width"
-                                    min="0"
-                                    max="10"
-                                    step="0.5"
-                                    label-at-start
-                                    placement="top"
-                                    value={sanitizeSliderValue(element.border?.thickness, 1, {min: 0, max: 10})}
-                                    onInput={(e) => updateValue(
-                                        'border.thickness',
-                                        sanitizeSliderValue(e.target.value, 1, {min: 0, max: 10}),
-                                    )}
-                                />
-                            </div>
-                            <div className="drawer-horizontal-element lgs-widget-border-control">
-                                <WaSlider
-                                    withTooltip
-                                    size="s"
-                                    label="Opacity"
-                                    min="0"
-                                    max="1"
-                                    step="0.05"
-                                    label-at-start
-                                    placement="top"
-                                    valueFormatter={formatSliderPercent}
-                                    value={borderOpacity}
-                                    onInput={(e) => updateValue(
-                                        'border.opacity',
-                                        sanitizeSliderValue(e.target.value, borderOpacity, {min: 0, max: 1}),
-                                    )}
-                                />
-                            </div>
-                        </div>
+                <div>
+                    <WaColorPicker
+                        size="s"
+                        swatches={swatches}
+                        value={getColor(element.border)}
+                        onInput={(e) => updateValue('border.color', e.target.value)}
+                    />
+                    <div>
+                        <WaSlider
+                            half-width
+                            withTooltip
+                            size="s"
+                            label="Width"
+                            min="0"
+                            max="10"
+                            step="0.5"
+                            label-at-start
+                            placement="top"
+                            value={sanitizeSliderValue(element.border?.thickness, 1, {min: 0, max: 10})}
+                            onInput={(e) => updateValue(
+                                'border.thickness',
+                                sanitizeSliderValue(e.target.value, 1, {min: 0, max: 10}),
+                            )}
+                        />
+                        <WaSlider
+                            half-width
+                            withTooltip
+                            size="s"
+                            label="Opacity"
+                            min="0"
+                            max="1"
+                            step="0.05"
+                            label-at-start
+                            placement="top"
+                            valueFormatter={formatSliderPercent}
+                            value={borderOpacity}
+                            onInput={(e) => updateValue(
+                                'border.opacity',
+                                sanitizeSliderValue(e.target.value, borderOpacity, {min: 0, max: 1}),
+                            )}
+                        />
+
                         {showScale && (
-                            <>
-                                <div className="lgs-widget-color-control-spacer" aria-hidden="true"/>
-                                <ScaleSwitchElement
-                                    checked={element.border?.scaled ?? false}
-                                    onChange={(checked) => updateValue('border.scaled', checked)}
-                                    className="lgs-widget-color-control-scaled-line lgs-widget-border-scaled-line"
-                                    widthAuto
-                                />
-                            </>
+                            <ScaleSwitchElement
+                                checked={element.border?.scaled ?? false}
+                                onChange={(checked) => updateValue('border.scaled', checked)}
+                            />
+                        )}
+
+                        {showRadius && (
+                            <RadiusElement
+                                element={element}
+                                updateValue={updateValue}
+                                showPill={showPill}
+                                showScale={showRadiusScale}
+                            />
                         )}
                     </div>
-
-                    {showRadius && (
-                        <>
-                            <div className="drawer-horizontal-line lgs-widget-border-radius-line">
-                                <WaSelect
-                                    size="s"
-                                    label={'Radius'}
-                                    label-at-start
-                                    style={{marginLeft: 'auto', width: '10rem'}}
-                                    value={currentRadius}
-                                    onChange={handleRadiusChange}
-                                >
-                                    {[...WIDGET_RADIUS.entries()].map(([_key, _data]) => {
-                                        if (!showPill && _key === 'pill') {
-                                            return null
-                                        }
-                                        return (
-                                            <WaOption key={_key} value={_key}>
-                                                {_data.name}
-                                            </WaOption>
-                                        )
-                                    })}
-                                </WaSelect>
-                            </div>
-                            {showRadiusScale && (
-                                <ScaleSwitchElement
-                                    checked={element.border?.radiusScaled ?? false}
-                                    onChange={(checked) => updateValue('border.radiusScaled', checked)}
-                                    className="lgs-widget-radius-scaled-line lgs-widget-border-radius-scaled-line"
-                                />
-                            )}
-                        </>
-                    )}
-                </>
+                </div>
             )}
         </div>
     )
--- src/components/MainUI/widgets/editor/elements/ScaleSwitchElement.jsx
+++ src/components/MainUI/widgets/editor/elements/ScaleSwitchElement.jsx
@@ -7,8 +7,8 @@
  * Author : LGS1920 Team
  * email: contact@lgs1920.fr
  *
- * Created on: 2026-06-09
- * Last modified: 2026-06-09
+ * Created on: 2026-06-18
+ * Last modified: 2026-06-18
  *
  *
  * Copyright © 2026 LGS1920
@@ -19,7 +19,6 @@ import { WaSwitch } from '@web.awesome.me/webawesome-pro/dist/react'
 export const ScaleSwitchElement = ({
                                        checked = false,
                                        onChange,
-                                       alignAfterColor = false,
                                        className = '',
                                        switchStyle,
                                        widthAuto = false,
@@ -27,10 +26,8 @@ export const ScaleSwitchElement = ({
     const stopPropagation = event => event.stopPropagation()
 
     return (
-        <div className={`drawer-horizontal-line lgs-widget-scaled-line ${className}`.trim()}>
-            {alignAfterColor && <div className="lgs-widget-scaled-color-spacer" aria-hidden="true"/>}
             <WaSwitch
-                className="lgs-widget-scaled-switch"
+                className={`lgs-widget-scaled-switch ${className}`.trim()}
                 style={switchStyle}
                 label-at-start
                 width-auto={widthAuto || undefined}
@@ -45,6 +42,5 @@ export const ScaleSwitchElement = ({
             >
                 <span>{'Scaled'}</span>
             </WaSwitch>
-        </div>
     )
 }
