dab4fdaae2e382fa6a8880643458454e1eb1db34
chdenat <christian.denat@orange.fr>
2026-07-08 12:35:13 +0200

    Refine logo arrow styling

--- src/components/MainUI/WelcomeModal.jsx
+++ src/components/MainUI/WelcomeModal.jsx
@@ -7,8 +7,8 @@
  * Author : LGS1920 Team
  * email: contact@lgs1920.fr
  *
- * Created on: 2026-05-10
- * Last modified: 2026-05-10
+ * Created on: 2026-07-08
+ * Last modified: 2026-07-08
  *
  *
  * Copyright © 2026 LGS1920
