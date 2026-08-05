47cd284b760eafc05ac3946e60089777e6b651d0
chdenat <christian.denat@orange.fr>
2026-08-05 12:40:53 +0200

    feat: configure journey stats date-time layout

--- src/components/DateTimeDisplay.jsx
+++ src/components/DateTimeDisplay.jsx
@@ -76,6 +76,7 @@ export const DateTimeDisplay = ({
                                     separator = '-',
                                     forceStack = false,
                                     stackItems = null,
+                                    stackDateTime = null,
                                     leading = null,
                                 }) => {
     const rootRef = useRef(null)
@@ -91,7 +92,7 @@ export const DateTimeDisplay = ({
     const sameDateRange = isSameDateRange(normalizedItems)
     const hasLeading = Boolean(leading)
     const shouldStackItems = !sameDateRange && (forceStack || (stackItems ?? normalizedItems.length > 1))
-    const shouldStackDateTime = forceStack || autoStack
+    const shouldStackDateTime = forceStack || (stackDateTime ?? autoStack)
 
     useLayoutEffect(() => {
         if (forceStack || normalizedItems.length === 0) {
@@ -122,7 +123,7 @@ export const DateTimeDisplay = ({
         resizeObserver.observe(rootRef.current)
 
         return () => resizeObserver.disconnect()
-    }, [forceStack, normalizedItems])
+    }, [forceStack, normalizedItems, stackDateTime])
 
     if (normalizedItems.length === 0) {
         return null
