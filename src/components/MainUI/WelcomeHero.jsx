36111b8e7d8ab9b14b1694504ee5bd9ea47aa156
chdenat <christian.denat@orange.fr>
2026-09-20 12:12:15 +0200

    fix(startup): match splash slogan rendering

--- src/components/MainUI/WelcomeHero.jsx
+++ src/components/MainUI/WelcomeHero.jsx
@@ -14,7 +14,6 @@
  * Copyright © 2026 LGS1920
  ******************************************************************************/
 
-import { SloganSvg }                                         from '@Components/MainUI/SloganSvg'
 import { WelcomeHeroControls }                               from '@Components/MainUI/WelcomeHeroControls'
 import { WelcomeHeroRoute }                                  from '@Components/MainUI/WelcomeHeroRoute'
 import {
@@ -23,6 +22,7 @@ import {
     getBannerMediaSource,
     WELCOME_BACKGROUND_PLAYBACK_RATE,
 }                                                               from '@Assets/media/welcome-background-media'
+import { SLOGAN }                                             from '@Core/constants'
 import { formatBuildInfo }                                    from '@Utils/BuildInfoUtils'
 import {
     WaButton, WaFormatDate, WaIcon,
@@ -278,7 +278,9 @@ export const WelcomeHero = ({
                         <source media="(max-width: 700px)" srcSet="/assets/logo/logo-vertical.png"/>
                         <img src="/assets/logo/logo-horizontal.png" alt="LGS1920 Studio logo"/>
                     </picture>
-                    <SloganSvg className="welcome-slogan"/>
+                    <div className="welcome-slogan" role="img" aria-label="Slogan">
+                        {SLOGAN}
+                    </div>
 
                     <div id="welcome-enter-call-for-action" className="welcome-enter-call-for-action">
                         <WaButton
